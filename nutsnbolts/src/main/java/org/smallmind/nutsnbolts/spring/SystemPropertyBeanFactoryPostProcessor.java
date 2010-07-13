7d3c7300ee12185249f4a4f72d654b39eed4d8e7
david <david@identitymind.com>
2010-07-13 14:26:48 -0700

    property init needs to be a factory level post processor

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyBeanFactoryPostProcessor.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyBeanFactoryPostProcessor.java
@@ -1,31 +1,34 @@
 package org.smallmind.nutsnbolts.spring;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.beans.factory.InitializingBean;
 
-public class SystemPropertyInitializingBean implements InitializingBean {
+public class SystemPropertyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
    private HashMap<String, String> propertyMap;
    private boolean override;
 
-   public SystemPropertyInitializingBean () {
+   public SystemPropertyBeanFactoryPostProcessor() {
 
       propertyMap = new HashMap<String, String>();
       override = false;
    }
 
-   public void setOverride (boolean override) {
+   public void setOverride(boolean override) {
 
       this.override = override;
    }
 
-   public void setPropertyMap (HashMap<String, String> propertyMap) {
+   public void setPropertyMap(HashMap<String, String> propertyMap) {
 
       this.propertyMap.putAll(propertyMap);
    }
 
-   public void afterPropertiesSet () {
+   public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
 
       for (Map.Entry<String, String> propertyEntry : propertyMap.entrySet()) {
          if (override || ((System.getProperty(propertyEntry.getKey()) == null) && (System.getenv(propertyEntry.getKey()) == null))) {
