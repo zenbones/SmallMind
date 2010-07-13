982d3e165db8cdc0f8a326f2452aa6234f58ce3a
david <david@identitymind.com>
2010-07-13 15:07:59 -0700

    allow system prop init to be priority ordered

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyBeanFactoryPostProcessor.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyBeanFactoryPostProcessor.java
@@ -3,19 +3,27 @@ package org.smallmind.nutsnbolts.spring;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class SystemPropertyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+public class SystemPropertyBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered, PriorityOrdered {
 
    private HashMap<String, String> propertyMap;
    private boolean override;
+   private int order;
 
    public SystemPropertyBeanFactoryPostProcessor() {
 
       propertyMap = new HashMap<String, String>();
       override = false;
+   }
+
+   public void setOrder(int order) {
+
+      this.order = order;
    }
 
    public void setOverride(boolean override) {
@@ -26,6 +34,11 @@ public class SystemPropertyBeanFactoryPostProcessor implements BeanFactoryPostPr
    public void setPropertyMap(HashMap<String, String> propertyMap) {
 
       this.propertyMap.putAll(propertyMap);
+   }
+
+   public int getOrder() {
+
+      return order;
    }
 
    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
