8ef5446c6ae74b5baf1ab99e3e7595c164b3959f
david <david@identitymind.com>
2010-09-08 17:15:49 -0700

    externallink fix

--- wicket/src/main/java/org/smallmind/wicket/component/link/ExternalLink.java
+++ wicket/src/main/java/org/smallmind/wicket/component/link/ExternalLink.java
@@ -54,7 +54,6 @@ public class ExternalLink extends AbstractLink {
    protected void onComponentTag (ComponentTag tag) {
 
       super.onComponentTag(tag);
-      checkComponentTag(tag, "img");
 
       tag.put("src", ((WebApplication)getApplication()).getServletContext().getContextPath() + tag.getString("src"));
 
