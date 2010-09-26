22a2a47123af3d4fab76ad7ec6a9034e3c20ebea
david <david@identitymind.com>
2010-09-26 02:37:49 -0700

    re-copyright springlayout

--- swing/src/main/java/org/smallmind/swing/SpringLayout.java
+++ swing/src/main/java/org/smallmind/swing/SpringLayout.java
@@ -24,24 +24,6 @@
  * alone subject to any of the requirements of the GNU Affero GPL
  * version 3.
  */
- *
- * Copyright (c) 1997 Sun Microsystems, Inc. All Rights Reserved.
- *
- * This software is the confidential and proprietary information of Sun
- * Microsystems, Inc. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Sun.
- *
- * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
- *
- */
-
 package org.smallmind.swing;
 
 import java.awt.Component;
