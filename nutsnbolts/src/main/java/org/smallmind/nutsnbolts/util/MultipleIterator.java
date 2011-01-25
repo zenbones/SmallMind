089cd5787ffa5d39c50fded83710b3d36e59b90d
David Berkman <david.berkman@grenadefish.net>
2011-01-25 11:53:02 -0800

    multiple iterator

---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/MultipleIterator.java
@@ -0,0 +1,12 @@
+package org.smallmind.nutsnbolts.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 1/25/11
+ * Time: 11:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultipleIterator {
+
+}
