d3acae1ab4d40d531c9613744d6574d9da32ded9
David Berkman <david.berkman@grenadefish.net>
2011-01-25 11:53:25 -0800

    multiple iterator

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/MultipleIterator.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/MultipleIterator.java
@@ -1,12 +1,57 @@
 package org.smallmind.nutsnbolts.util;
 
-/**
- * Created by IntelliJ IDEA.
- * User: david.berkman
- * Date: 1/25/11
- * Time: 11:39 AM
- * To change this template use File | Settings | File Templates.
- */
-public class MultipleIterator {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+public class MultipleIterator<T> implements Iterator<T>, Iterable<T> {
+
+   private Iterator<T>[] iterators;
+   private int index = 0;
+
+   public MultipleIterator (Iterator<T>... iterators) {
+
+      this.iterators = iterators;
+
+      moveIndex();
+   }
+
+   private void moveIndex () {
+
+      while ((index < iterators.length) && (!iterators[index].hasNext())) {
+         index++;
+      }
+   }
+
+   public Iterator<T> iterator () {
+
+      return this;
+   }
+
+   public boolean hasNext () {
+
+      return index < iterators.length;
+   }
+
+   public T next () {
+
+      if (!hasNext()) {
+         throw new NoSuchElementException();
+      }
+
+      try {
+         return iterators[index].next();
+      }
+      finally {
+         moveIndex();
+      }
+   }
+
+   public void remove () {
+
+      if (!(index < iterators.length)) {
+         throw new IllegalStateException("The next() method has not been called");
+      }
+
+      iterators[index].remove();
+   }
 }
