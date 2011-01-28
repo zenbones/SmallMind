55276870b7d67f882a571b35a356b77324d65c7e
David Berkman <david.berkman@grenadefish.net>
2011-01-27 18:00:51 -0800

    rework of HTTPTransmitter

---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/http/HttpPipe.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
+ * 
+ * This file is part of the SmallMind Code Project.
+ * 
+ * The SmallMind Code Project is free software, you can redistribute
+ * it and/or modify it under the terms of GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The SmallMind Code Project is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the the GNU Affero General Public
+ * License, along with The SmallMind Code Project. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under the GNU Affero GPL version 3 section 7
+ * ------------------------------------------------------------------
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with other code, such other code is not for that reason
+ * alone subject to any of the requirements of the GNU Affero GPL
+ * version 3.
+ */
+package org.smallmind.nutsnbolts.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import org.smallmind.nutsnbolts.lang.UnknownSwitchCaseException;
+
+public class HttpPipe {
+
+   private static enum State {WRITE, READ, TERMINATED}
+
+   private HttpURLConnection urlConnection;
+   private OutputStream httpOutput;
+   private InputStream httpInput;
+   private State state;
+
+   public HttpPipe (HttpURLConnection urlConnection)
+      throws IOException {
+
+      this.urlConnection = urlConnection;
+
+      urlConnection.connect();
+      state = urlConnection.getDoOutput() ? State.WRITE : urlConnection.getDoInput() ? State.READ : State.TERMINATED;
+
+      switch (state) {
+         case WRITE:
+            httpOutput = urlConnection.getOutputStream();
+            break;
+         case READ:
+            httpInput = urlConnection.getInputStream();
+            break;
+         case TERMINATED:
+            urlConnection.disconnect();
+            break;
+         default:
+            throw new UnknownSwitchCaseException(state.name());
+      }
+   }
+
+   public synchronized HttpPipe write (String body)
+      throws IOException {
+
+      if (!state.equals(State.WRITE)) {
+         throw new IllegalStateException("Pipe's state does not allow writing");
+      }
+
+      httpOutput.write(body.getBytes());
+      httpOutput.flush();
+
+      return this;
+   }
+
+   public synchronized void doneWriting ()
+      throws IOException {
+
+      if (!state.equals(State.WRITE)) {
+         throw new IllegalStateException("Pipe's state does not allow writing");
+      }
+
+      httpOutput.close();
+
+      state = urlConnection.getDoInput() ? State.READ : State.TERMINATED;
+
+      if (state.equals(State.READ)) {
+         httpInput = urlConnection.getInputStream();
+      }
+      else {
+         urlConnection.disconnect();
+      }
+   }
+
+   public synchronized int read (byte[] buffer)
+      throws IOException {
+
+      int bytesRead = 0;
+
+      if (!state.equals(State.READ)) {
+         throw new IllegalStateException("Pipe's state does not allow reading");
+      }
+
+      try {
+         return (bytesRead = httpInput.read(buffer));
+      }
+      finally {
+         if (bytesRead < 0) {
+            doneReading();
+         }
+      }
+   }
+
+   public synchronized void doneReading ()
+      throws IOException {
+
+      if (!state.equals(State.READ)) {
+         throw new IllegalStateException("Pipe's state does not allow reading");
+      }
+
+      state = State.TERMINATED;
+
+      try {
+         httpInput.close();
+      }
+      finally {
+         urlConnection.disconnect();
+      }
+   }
+}
--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/http/HttpTransmitter.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/http/HttpTransmitter.java
@@ -26,90 +26,65 @@
  */
 package org.smallmind.nutsnbolts.http;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.smallmind.nutsnbolts.util.Tuple;
+import org.smallmind.nutsnbolts.lang.UnknownSwitchCaseException;
 
 public class HttpTransmitter {
 
-   public static void emitGet (String cgiUrlString)
+   public static HttpPipe emitGetRequest (URL url, boolean openReader)
       throws IOException {
 
-      emitGet(cgiUrlString, null);
+      return emitHttpRequest(HttpMethod.GET, url, openReader);
    }
 
-   public static void emitGet (String cgiUrlString, PrintWriter out)
+   public static HttpPipe emitPutRequest (URL url, boolean openReader)
       throws IOException {
 
-      emitHttpRequest(cgiUrlString, null, out);
+      return emitHttpRequest(HttpMethod.PUT, url, openReader);
    }
 
-   public static void emitPost (String cgiUrlString, Tuple<String, String> tuple)
+   public static HttpPipe emitPostRequest (URL url, boolean openReader)
       throws IOException {
 
-      emitPost(cgiUrlString, tuple, null);
+      return emitHttpRequest(HttpMethod.POST, url, openReader);
    }
 
-   public static void emitPost (String cgiUrlString, Tuple<String, String> tuple, PrintWriter out)
+   public static HttpPipe emitDeleteRequest (URL url, boolean openReader)
       throws IOException {
 
-      emitHttpRequest(cgiUrlString, tuple, out);
+      return emitHttpRequest(HttpMethod.DELETE, url, openReader);
    }
 
-   private static void emitHttpRequest (String cgiUrlString, Tuple<String, String> tuple, PrintWriter out)
+   public static HttpPipe emitHttpRequest (HttpMethod method, URL url, boolean openReader)
       throws IOException {
 
-      URL cgiUrl;
-      HttpURLConnection cgiConnect;
-      InputStream postInputStream;
-      BufferedReader postReader;
-      OutputStream postOutputStream;
-      BufferedWriter postWriter;
-      String encodedPostData = null;
-      String singleLine;
+      HttpURLConnection urlConnection;
 
-      cgiUrl = new URL(cgiUrlString);
-      cgiConnect = (HttpURLConnection)cgiUrl.openConnection();
-      if (tuple != null) {
-         encodedPostData = HTTPCodec.urlEncode(tuple);
-         cgiConnect.setDoOutput(true);
-         cgiConnect.setRequestMethod("POST");
+      urlConnection = (HttpURLConnection)url.openConnection();
+      urlConnection.setDoInput(openReader);
+      switch (method) {
+         case GET:
+            urlConnection.setDoOutput(false);
+            urlConnection.setRequestMethod("GET");
+            break;
+         case PUT:
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("PUT");
+            break;
+         case POST:
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            break;
+         case DELETE:
+            urlConnection.setDoOutput(false);
+            urlConnection.setRequestMethod("DELETE");
+            break;
+         default:
+            throw new UnknownSwitchCaseException(method.name());
       }
-      else {
-         cgiConnect.setDoOutput(false);
-         cgiConnect.setRequestMethod("GET");
-      }
-      if (out != null) {
-         cgiConnect.setDoInput(true);
-      }
-      else {
-         cgiConnect.setDoInput(false);
-      }
-      cgiConnect.connect();
-      if (tuple != null) {
-         postOutputStream = cgiConnect.getOutputStream();
-         postWriter = new BufferedWriter(new OutputStreamWriter(postOutputStream));
-         postWriter.write(encodedPostData);
-         postWriter.flush();
-         postWriter.close();
-      }
-      if (out != null) {
-         postInputStream = cgiConnect.getInputStream();
-         postReader = new BufferedReader(new InputStreamReader(postInputStream));
-         while ((singleLine = postReader.readLine()) != null) {
-            out.println(singleLine);
-         }
-         postReader.close();
-      }
-      cgiConnect.disconnect();
+
+      return new HttpPipe(urlConnection);
    }
-
 }
