ef905a7b2892bb5cd565f94d37962bc7c133807f
David Berkman <david.berkman@grenadefish.net>
2011-03-07 18:01:26 -0800

    better error message

--- persistence/src/main/java/org/smallmind/persistence/cache/DurableKeyConcurrentList.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/DurableKeyConcurrentList.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -73,7 +73,14 @@ public class DurableKeyConcurrentList<I extends Serializable & Comparable<I>, D 
   private ORMDao<I, D> getORMDao () {
 
     if (!ormReferenced) {
-      if (ormDaoRef.compareAndSet(null, DaoManager.get(durableClass))) {
+
+      ORMDao<I, D> ormDao;
+
+      if ((ormDao = DaoManager.get(durableClass)) == null) {
+        throw new CacheOperationException("Unable to locate an implementation of ORMDao within DaoManager for the requested durable(%s)", durableClass.getSimpleName());
+      }
+
+      if (ormDaoRef.compareAndSet(null, ormDao)) {
         ormReferenced = true;
       }
     }
