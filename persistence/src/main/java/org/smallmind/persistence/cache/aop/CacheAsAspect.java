6c69b8c592ee00eb349d4bb50953d00eca4755c7
David Berkman <david.berkman@grenadefish.net>
2011-03-11 19:11:14 -0800

    fix for when the cache is defined but turned off

--- persistence/src/main/java/org/smallmind/persistence/cache/aop/CacheAsAspect.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/aop/CacheAsAspect.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -49,174 +49,176 @@ import org.smallmind.persistence.statistics.aop.StatisticsStopwatch;
 @Aspect
 public class CacheAsAspect {
 
-   private static final Random RANDOM = new SecureRandom();
-   private static final StatisticsFactory STATISTICS_FACTORY = PersistenceManager.getPersistence().getStatisticsFactory();
+  private static final Random RANDOM = new SecureRandom();
+  private static final StatisticsFactory STATISTICS_FACTORY = PersistenceManager.getPersistence().getStatisticsFactory();
 
-   @Around(value = "execution(@CacheAs * * (..)) && @annotation(cacheAs) && this(ormDao)", argNames = "thisJoinPoint, cacheAs, ormDao")
-   public Object aroundCacheAsMethod (ProceedingJoinPoint thisJoinPoint, CacheAs cacheAs, CacheAwareORMDao ormDao)
-      throws Throwable {
+  @Around(value = "execution(@CacheAs * * (..)) && @annotation(cacheAs) && this(ormDao)", argNames = "thisJoinPoint, cacheAs, ormDao")
+  public Object aroundCacheAsMethod (ProceedingJoinPoint thisJoinPoint, CacheAs cacheAs, CacheAwareORMDao ormDao)
+    throws Throwable {
 
-      Annotation statisticsStopwatchAnnotation;
-      Method executedMethod = null;
-      String statisticsSource = null;
-      boolean timingEnabled;
-      long start = 0;
-      long stop;
+    Annotation statisticsStopwatchAnnotation;
+    Method executedMethod = null;
+    String statisticsSource = null;
+    boolean timingEnabled;
+    long start = 0;
+    long stop;
 
-      statisticsStopwatchAnnotation = ormDao.getClass().getAnnotation(StatisticsStopwatch.class);
-      if (timingEnabled = STATISTICS_FACTORY.isEnabled() && (statisticsStopwatchAnnotation != null) && ((StatisticsStopwatch)statisticsStopwatchAnnotation).value()) {
-         start = System.currentTimeMillis();
+    statisticsStopwatchAnnotation = ormDao.getClass().getAnnotation(StatisticsStopwatch.class);
+    if (timingEnabled = STATISTICS_FACTORY.isEnabled() && (statisticsStopwatchAnnotation != null) && ((StatisticsStopwatch)statisticsStopwatchAnnotation).value()) {
+      start = System.currentTimeMillis();
+    }
+
+    try {
+
+      Type returnType;
+
+      if (cacheAs.time().value() < 0) {
+        throw new CacheAutomationError("The base time(%d) value of a @CacheAs annotation can not be negative", cacheAs.time().value());
       }
 
-      try {
-
-         Type returnType;
-
-         if (cacheAs.time().value() < 0) {
-            throw new CacheAutomationError("The base time(%d) value of a @CacheAs annotation can not be negative", cacheAs.time().value());
-         }
-
-         if (cacheAs.time().stochastic() < 0) {
-            throw new CacheAutomationError("The stochastic(%d) attribute of a @CacheAs annotation can not be negative", cacheAs.time().stochastic());
-         }
-
-         if (cacheAs.singular()) {
-            if (cacheAs.ordered()) {
-               throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) can't be ordered", cacheAs.comparator().getClass().getName());
-            }
-            else if (cacheAs.max() > 0) {
-               throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) may not define a maximum size", cacheAs.comparator().getClass().getName());
-            }
-            else if (!cacheAs.comparator().equals(Comparator.class)) {
-               throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) can not register a comparator(%s)", cacheAs.comparator().getClass().getName());
-            }
-
-            if (!ormDao.getManagedClass().equals(((MethodSignature)thisJoinPoint.getSignature()).getReturnType())) {
-               throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) must return its managed type(%s)", ormDao.getManagedClass().getSimpleName());
-            }
-
-            VectorKey vectorKey;
-            VectoredDao vectoredDao = ormDao.getVectoredDao();
-
-            vectorKey = new VectorKey(VectorIndices.getVectorIndexes(cacheAs.value(), thisJoinPoint, ormDao), ormDao.getManagedClass(), Classifications.get(CacheAs.class, thisJoinPoint, cacheAs.value()));
-
-            if (vectoredDao != null) {
-
-               DurableVector vector;
-
-               if ((vector = vectoredDao.getVector(vectorKey)) != null) {
-                  if (!vector.isAlive()) {
-                     vectoredDao.deleteVector(vectorKey);
-                  }
-                  else {
-                     statisticsSource = vectoredDao.getStatisticsSource();
-
-                     return vector.head();
-                  }
-               }
-            }
-
-            Durable durable;
-
-            statisticsSource = ormDao.getStatisticsSource();
-            vectoredDao.lookupLock();
-            try {
-               if ((durable = (Durable)thisJoinPoint.proceed()) != null) {
-                  if (vectoredDao != null) {
-
-                     DurableVector vector = vectoredDao.createSingularVector(vectorKey, durable, getTimeToLive(cacheAs));
-
-                     return vectoredDao.persistVector(vectorKey, vector).head();
-                  }
-               }
-
-               return durable;
-            }
-            finally {
-               vectoredDao.lookupUnlock();
-            }
-         }
-         else {
-            if ((!cacheAs.comparator().equals(Comparator.class)) && (!cacheAs.ordered())) {
-               throw new CacheAutomationError("A method annotated with @CacheAs has registered a comparator(%s) but is not ordered", cacheAs.comparator().getClass().getName());
-            }
-
-            if (!Iterable.class.isAssignableFrom(((MethodSignature)thisJoinPoint.getSignature()).getReturnType())) {
-               throw new CacheAutomationError("Methods annotated with @CacheAs (singular = false) must return a value of type <? extends Iterable>");
-            }
-
-            if ((!((returnType = (executedMethod = ((MethodSignature)thisJoinPoint.getSignature()).getMethod()).getGenericReturnType()) instanceof ParameterizedType)) || (!ormDao.getManagedClass().equals(((ParameterizedType)returnType).getActualTypeArguments()[0]))) {
-               throw new CacheAutomationError("Methods annotated with @CacheAs (singular = false) must return a value of type <? extends Iterable<%s>>", ormDao.getManagedClass().getSimpleName());
-            }
-
-            VectorKey vectorKey;
-            VectoredDao vectoredDao = ormDao.getVectoredDao();
-
-            vectorKey = new VectorKey(VectorIndices.getVectorIndexes(cacheAs.value(), thisJoinPoint, ormDao), ormDao.getManagedClass(), Classifications.get(CacheAs.class, thisJoinPoint, cacheAs.value()));
-
-            if (vectoredDao != null) {
-
-               DurableVector vector;
-
-               if ((vector = vectoredDao.getVector(vectorKey)) != null) {
-                  if (!vector.isAlive()) {
-                     vectoredDao.deleteVector(vectorKey);
-                  }
-                  else {
-                     statisticsSource = vectoredDao.getStatisticsSource();
-
-                     return vector.asList();
-                  }
-               }
-            }
-
-            Iterable iterable;
-
-            statisticsSource = ormDao.getStatisticsSource();
-            vectoredDao.lookupLock();
-            try {
-               if ((iterable = (Iterable)thisJoinPoint.proceed()) != null) {
-                  if (vectoredDao != null) {
-
-                     DurableVector vector = vectoredDao.createVector(vectorKey, iterable, cacheAs.comparator().equals(Comparator.class) ? null : cacheAs.comparator().newInstance(), cacheAs.max(), getTimeToLive(cacheAs), cacheAs.ordered());
-
-                     return vectoredDao.persistVector(vectorKey, vector).asList();
-                  }
-               }
-
-               return iterable;
-            }
-            finally {
-               vectoredDao.lookupUnlock();
-            }
-         }
-      }
-      finally {
-         if (timingEnabled) {
-            stop = System.currentTimeMillis();
-
-            if (executedMethod == null) {
-               executedMethod = ((MethodSignature)thisJoinPoint.getSignature()).getMethod();
-            }
-
-            STATISTICS_FACTORY.getStatistics().addStatLine(ormDao.getManagedClass(), executedMethod, statisticsSource, stop - start);
-         }
-      }
-   }
-
-   private long getTimeToLive (CacheAs cacheAs) {
-
-      if ((cacheAs.time().value() == 0) && (cacheAs.time().stochastic() == 0)) {
-
-         return 0;
+      if (cacheAs.time().stochastic() < 0) {
+        throw new CacheAutomationError("The stochastic(%d) attribute of a @CacheAs annotation can not be negative", cacheAs.time().stochastic());
       }
 
-      long baseTime = cacheAs.time().unit().toMillis(cacheAs.time().value());
+      if (cacheAs.singular()) {
+        if (cacheAs.ordered()) {
+          throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) can't be ordered", cacheAs.comparator().getClass().getName());
+        }
+        else if (cacheAs.max() > 0) {
+          throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) may not define a maximum size", cacheAs.comparator().getClass().getName());
+        }
+        else if (!cacheAs.comparator().equals(Comparator.class)) {
+          throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) can not register a comparator(%s)", cacheAs.comparator().getClass().getName());
+        }
 
-      if (cacheAs.time().stochastic() > 0) {
-         baseTime += (cacheAs.time().unit().toMillis(cacheAs.time().stochastic()) * RANDOM.nextDouble()) + 1;
+        if (!ormDao.getManagedClass().equals(((MethodSignature)thisJoinPoint.getSignature()).getReturnType())) {
+          throw new CacheAutomationError("A method annotated with @CacheAs (singular = true) must return its managed type(%s)", ormDao.getManagedClass().getSimpleName());
+        }
+
+        VectoredDao vectoredDao;
+
+        if ((vectoredDao = ormDao.getVectoredDao()) == null) {
+          statisticsSource = ormDao.getStatisticsSource();
+
+          return thisJoinPoint.proceed();
+        }
+        else {
+
+          VectorKey vectorKey;
+          DurableVector vector;
+
+          vectorKey = new VectorKey(VectorIndices.getVectorIndexes(cacheAs.value(), thisJoinPoint, ormDao), ormDao.getManagedClass(), Classifications.get(CacheAs.class, thisJoinPoint, cacheAs.value()));
+
+          if ((vector = vectoredDao.getVector(vectorKey)) != null) {
+            if (!vector.isAlive()) {
+              vectoredDao.deleteVector(vectorKey);
+            }
+            else {
+              statisticsSource = vectoredDao.getStatisticsSource();
+
+              return vector.head();
+            }
+          }
+
+          Durable durable;
+
+          statisticsSource = ormDao.getStatisticsSource();
+          vectoredDao.lookupLock();
+          try {
+            if ((durable = (Durable)thisJoinPoint.proceed()) != null) {
+
+              return vectoredDao.persistVector(vectorKey, vectoredDao.createSingularVector(vectorKey, durable, getTimeToLive(cacheAs))).head();
+            }
+
+            return null;
+          }
+          finally {
+            vectoredDao.lookupUnlock();
+          }
+        }
       }
+      else {
+        if ((!cacheAs.comparator().equals(Comparator.class)) && (!cacheAs.ordered())) {
+          throw new CacheAutomationError("A method annotated with @CacheAs has registered a comparator(%s) but is not ordered", cacheAs.comparator().getClass().getName());
+        }
 
-      return baseTime;
-   }
+        if (!Iterable.class.isAssignableFrom(((MethodSignature)thisJoinPoint.getSignature()).getReturnType())) {
+          throw new CacheAutomationError("Methods annotated with @CacheAs (singular = false) must return a value of type <? extends Iterable>");
+        }
+
+        if ((!((returnType = (executedMethod = ((MethodSignature)thisJoinPoint.getSignature()).getMethod()).getGenericReturnType()) instanceof ParameterizedType)) || (!ormDao.getManagedClass().equals(((ParameterizedType)returnType).getActualTypeArguments()[0]))) {
+          throw new CacheAutomationError("Methods annotated with @CacheAs (singular = false) must return a value of type <? extends Iterable<%s>>", ormDao.getManagedClass().getSimpleName());
+        }
+
+        VectoredDao vectoredDao;
+
+        if ((vectoredDao = ormDao.getVectoredDao()) == null) {
+          statisticsSource = ormDao.getStatisticsSource();
+
+          return thisJoinPoint.proceed();
+        }
+        else {
+
+          VectorKey vectorKey;
+          DurableVector vector;
+
+          vectorKey = new VectorKey(VectorIndices.getVectorIndexes(cacheAs.value(), thisJoinPoint, ormDao), ormDao.getManagedClass(), Classifications.get(CacheAs.class, thisJoinPoint, cacheAs.value()));
+
+          if ((vector = vectoredDao.getVector(vectorKey)) != null) {
+            if (!vector.isAlive()) {
+              vectoredDao.deleteVector(vectorKey);
+            }
+            else {
+              statisticsSource = vectoredDao.getStatisticsSource();
+
+              return vector.asList();
+            }
+          }
+
+          Iterable iterable;
+
+          statisticsSource = ormDao.getStatisticsSource();
+          vectoredDao.lookupLock();
+          try {
+            if ((iterable = (Iterable)thisJoinPoint.proceed()) != null) {
+
+              return vectoredDao.persistVector(vectorKey, vectoredDao.createVector(vectorKey, iterable, cacheAs.comparator().equals(Comparator.class) ? null : cacheAs.comparator().newInstance(), cacheAs.max(), getTimeToLive(cacheAs), cacheAs.ordered())).asList();
+            }
+
+            return null;
+          }
+          finally {
+            vectoredDao.lookupUnlock();
+          }
+        }
+      }
+    }
+    finally {
+      if (timingEnabled) {
+        stop = System.currentTimeMillis();
+
+        if (executedMethod == null) {
+          executedMethod = ((MethodSignature)thisJoinPoint.getSignature()).getMethod();
+        }
+
+        STATISTICS_FACTORY.getStatistics().addStatLine(ormDao.getManagedClass(), executedMethod, statisticsSource, stop - start);
+      }
+    }
+  }
+
+  private long getTimeToLive (CacheAs cacheAs) {
+
+    if ((cacheAs.time().value() == 0) && (cacheAs.time().stochastic() == 0)) {
+
+      return 0;
+    }
+
+    long baseTime = cacheAs.time().unit().toMillis(cacheAs.time().value());
+
+    if (cacheAs.time().stochastic() > 0) {
+      baseTime += (cacheAs.time().unit().toMillis(cacheAs.time().stochastic()) * RANDOM.nextDouble()) + 1;
+    }
+
+    return baseTime;
+  }
 }
