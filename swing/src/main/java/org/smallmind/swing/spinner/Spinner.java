32d034773e7dc6ed166ac642824c275014753a92
David Berkman <david.berkman@grenadefish.net>
2011-04-02 11:21:11 -0700

    spinner

--- swing/src/main/java/org/smallmind/swing/spinner/Spinner.java
+++ swing/src/main/java/org/smallmind/swing/spinner/Spinner.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -133,6 +133,8 @@ public class Spinner extends JPanel implements EditorListener, ActionListener, C
     spinnerDownButtonRepeater.addActionListener(this);
     rubberStamp.addMouseListener(this);
     spinnerModel.addChangeListener(this);
+
+    enableSpinning();
   }
 
   public synchronized void addChangeListener (ChangeListener changeListener) {
@@ -145,7 +147,7 @@ public class Spinner extends JPanel implements EditorListener, ActionListener, C
     listenerList.removeListener(changeListener);
   }
 
-  public synchronized SpinnerRenderer getSpinnerRednerer () {
+  public synchronized SpinnerRenderer getSpinnerRenderer () {
 
     return renderer;
   }
