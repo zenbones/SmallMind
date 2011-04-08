9d105d20984d657e37920a263605c04e6a59c602
David Berkman <david.berkman@grenadefish.net>
2011-04-07 23:46:57 -0700

    formula text field (expanding)

--- swing/src/main/java/org/smallmind/swing/text/FormulaTextField.java
+++ swing/src/main/java/org/smallmind/swing/text/FormulaTextField.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -46,7 +46,7 @@ import org.smallmind.nutsnbolts.lang.UnknownSwitchCaseException;
 import org.smallmind.nutsnbolts.util.WeakEventListenerList;
 import org.smallmind.swing.ComponentUtilities;
 
-public class ExpandingTextField extends JPanel implements ActionListener, DocumentListener {
+public class FormulaTextField extends JPanel implements ActionListener, DocumentListener {
 
   private static final ImageIcon COLLAPSE_ICON = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("org/smallmind/swing/system/navigate_open_16.png"));
   private static final ImageIcon EXPAND_ICON = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("org/smallmind/swing/system/navigate_close_16.png"));
@@ -62,22 +62,22 @@ public class ExpandingTextField extends JPanel implements ActionListener, Docume
   private JTextField contractedTextField;
   private AtomicBoolean documentSensitive = new AtomicBoolean(true);
 
-  public ExpandingTextField () {
+  public FormulaTextField () {
 
     this(null);
   }
 
-  public ExpandingTextField (String text) {
+  public FormulaTextField (String text) {
 
     this(text, 5);
   }
 
-  public ExpandingTextField (int rows) {
+  public FormulaTextField (int rows) {
 
     this(null, rows);
   }
 
-  public ExpandingTextField (String text, int rows) {
+  public FormulaTextField (String text, int rows) {
 
     JPanel contractedPanel;
     JPanel expandedPanel;
