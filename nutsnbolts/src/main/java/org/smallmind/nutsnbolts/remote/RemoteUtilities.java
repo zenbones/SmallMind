6a12c55bc6c28cff89d1cd1c399b73f13eaeb485
David Berkman <david.berkman@grenadefish.net>
2011-04-13 12:58:08 -0700

    moved RemoteUtilities and upgraded version

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/remote/RemoteUtilities.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/remote/RemoteUtilities.java
@@ -24,7 +24,7 @@
  * alone subject to any of the requirements of the GNU Affero GPL
  * version 3.
  */
-package org.smallmind.quorum.transport.remote;
+package org.smallmind.nutsnbolts.remote;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
