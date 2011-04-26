6270073ad0e02764056163a86e860dcdddfe8aae
David Berkman <david.berkman@grenadefish.net>
2011-04-25 17:29:32 -0700

    more readable iconography

--- swing/src/main/java/org/smallmind/swing/text/FormulaTextField.java
+++ swing/src/main/java/org/smallmind/swing/text/FormulaTextField.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -54,6 +54,7 @@ public class FormulaTextField extends JPanel implements ActionListener, ItemList
   private static final ImageIcon COLLAPSE_ICON = new ImageIcon(ClassLoader.getSystemResource("org/smallmind/swing/system/navigate_open_16.png"));
   private static final ImageIcon EXPAND_ICON = new ImageIcon(ClassLoader.getSystemResource("org/smallmind/swing/system/navigate_close_16.png"));
   private static final ImageIcon FORMULA_ICON = new ImageIcon(ClassLoader.getSystemResource("org/smallmind/swing/system/text_formula_16.png"));
+  private static final ImageIcon TEXT_ICON = new ImageIcon(ClassLoader.getSystemResource("org/smallmind/swing/system/text_marked_16.png"));
 
   private static enum CardState {COLLAPSED, EXPANDED}
 
@@ -135,7 +136,8 @@ public class FormulaTextField extends JPanel implements ActionListener, ItemList
     collapsedFormulaButton.setToolTipText("evaluate as text/formula");
     collapsedFormulaButton.addItemListener(this);
 
-    expandedFormulaButton = new JToggleButton(FORMULA_ICON, formula);
+    expandedFormulaButton = new JToggleButton(TEXT_ICON, formula);
+    expandedFormulaButton.setSelectedIcon(FORMULA_ICON);
     expandedFormulaButton.setFocusable(false);
     expandedFormulaButton.setToolTipText("evaluate as text/formula");
     expandedFormulaButton.addItemListener(this);
