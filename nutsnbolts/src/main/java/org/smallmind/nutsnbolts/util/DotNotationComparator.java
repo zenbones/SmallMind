b009265f044145b096778a5cd9be5c3b1f44dd63
David Berkman <david.berkman@grenadefish.net>
2011-05-10 13:21:51 -0700

    dot notation comparator fix

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/DotNotationComparator.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/DotNotationComparator.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -32,6 +32,18 @@ public class DotNotationComparator implements Comparator<String> {
 
   private static final AlphaNumericComparator<String> ALPHA_NUMERIC_COMPARATOR = new AlphaNumericComparator<String>();
 
+  private boolean reversed;
+
+  public DotNotationComparator () {
+
+    this(false);
+  }
+
+  public DotNotationComparator (boolean reversed) {
+
+    this.reversed = reversed;
+  }
+
   @Override
   public int compare (String string1, String string2) {
 
@@ -41,7 +53,7 @@ public class DotNotationComparator implements Comparator<String> {
     int comparison;
 
     for (int index = 0; index < commonSegments; index++) {
-      if ((comparison = ALPHA_NUMERIC_COMPARATOR.compare(string1, string2)) != 0) {
+      if ((comparison = ALPHA_NUMERIC_COMPARATOR.compare(segments1[reversed ? segments1.length - (index + 1) : index], segments2[reversed ? segments2.length - (index + 1) : index])) != 0) {
 
         return comparison;
       }
