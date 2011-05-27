d2f784717465ea2189a99b2eae7f23cce2b63079
David Berkman <david.berkman@grenadefish.net>
2011-05-27 13:56:14 -0700

    http pipe state improvement

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/http/HttpPipe.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/http/HttpPipe.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -34,12 +34,12 @@ import org.smallmind.nutsnbolts.lang.UnknownSwitchCaseException;
 
 public class HttpPipe {
 
-  private static enum State {WRITE, READ, TERMINATED}
+  private static enum State {DISCONNECTED, WRITE, READ, TERMINATED}
 
   private HttpURLConnection urlConnection;
   private OutputStream httpOutput;
   private InputStream httpInput;
-  private State state;
+  private State state = State.DISCONNECTED;
 
   public HttpPipe (HttpURLConnection urlConnection)
     throws IOException {
@@ -57,6 +57,13 @@ public class HttpPipe {
 
   public synchronized HttpPipe connect ()
     throws IOException {
+
+    if (state.equals(State.DISCONNECTED)) {
+      throw new IllegalStateException("Pipe has already been connected");
+    }
+    if (state.equals(State.TERMINATED)) {
+      throw new IllegalStateException("Pipe has already been disconnected");
+    }
 
     urlConnection.connect();
     state = urlConnection.getDoOutput() ? State.WRITE : urlConnection.getDoInput() ? State.READ : State.TERMINATED;
