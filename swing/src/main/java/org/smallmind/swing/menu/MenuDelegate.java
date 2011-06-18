30e689d9449cc3da91a51d8ac4d411a5952ba2d2
David Berkman <david.berkman@grenadefish.net>
2011-06-17 20:05:26 -0700

    menu handling

---
+++ swing/src/main/java/org/smallmind/swing/menu/MenuDelegate.java
@@ -0,0 +1,12 @@
+package org.smallmind.swing.menu;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 6/17/11
+ * Time: 7:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuDelegate {
+
+}
---
+++ swing/src/main/java/org/smallmind/swing/menu/MenuHandler.java
@@ -0,0 +1,12 @@
+package org.smallmind.swing.menu;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 6/17/11
+ * Time: 7:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuHandler {
+
+}
