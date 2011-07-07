e3d3ee739dc3f06941304d1db4a160664eadbddf
David Berkman <david.berkman@grenadefish.net>
2011-07-07 05:24:17 -0700

    minor optimization

--- persistence/src/main/java/org/smallmind/persistence/cache/concurrent/ConcurrentDurableVector.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/concurrent/ConcurrentDurableVector.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -65,12 +65,12 @@ public abstract class ConcurrentDurableVector<I extends Serializable & Comparabl
 
         Iterator<D> rosterIter = getRoster().iterator();
         D element;
-        boolean removed = false;
+        boolean matched = false;
         boolean zoned = false;
         boolean inserted = false;
         int index = 0;
 
-        while ((!(removed && zoned)) && rosterIter.hasNext()) {
+        while ((!(matched && zoned)) && rosterIter.hasNext()) {
           element = rosterIter.next();
 
           if (element.equals(durable)) {
@@ -82,9 +82,9 @@ public abstract class ConcurrentDurableVector<I extends Serializable & Comparabl
               rosterIter.remove();
             }
 
-            removed = true;
+            matched = true;
           }
-          else if (((getComparator() == null) ? element.compareTo(durable) : getComparator().compare(element, durable)) >= 0) {
+          else if ((!zoned) && ((getComparator() == null) ? element.compareTo(durable) : getComparator().compare(element, durable)) >= 0) {
             zoned = true;
           }
           else if (!zoned) {
