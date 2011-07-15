4f4fb1c6ce7c7c429d7659a6c0b07c1494a92fee
David Berkman <david.berkman@grenadefish.net>
2011-07-15 13:32:15 -0700

    durable serial version uid and upgrade to 1.1.15

--- persistence/src/main/java/org/smallmind/persistence/Durable.java
+++ persistence/src/main/java/org/smallmind/persistence/Durable.java
@@ -37,178 +37,181 @@ import org.terracotta.annotations.InstrumentedClass;
 @InstrumentedClass
 public abstract class Durable<I extends Comparable<I>> implements Serializable, Comparable<Durable<I>> {
 
-   private static final ThreadLocal<Set<Durable>> IN_USE_SET_LOCAL = new ThreadLocal<Set<Durable>>() {
+  private static final long serialVersionUID = 1L;
 
-      @Override
-      protected Set<Durable> initialValue () {
+  private static final ThreadLocal<Set<Durable>> IN_USE_SET_LOCAL = new ThreadLocal<Set<Durable>>() {
 
-         return new HashSet<Durable>();
-      }
-   };
+    @Override
+    protected Set<Durable> initialValue () {
 
-   private I id;
+      return new HashSet<Durable>();
+    }
+  };
 
-   public Durable () {
-   }
+  private I id;
 
-   public Durable (Durable<I> durable) {
+  public Durable () {
 
-      id = durable.getId();
-   }
+  }
 
-   @AutolockRead
-   public synchronized I getId () {
+  public Durable (Durable<I> durable) {
 
-      return id;
-   }
+    id = durable.getId();
+  }
 
-   @AutolockWrite
-   public synchronized void setId (I id) {
+  @AutolockRead
+  public synchronized I getId () {
 
-      this.id = id;
-   }
+    return id;
+  }
 
-   public int compareTo (Durable<I> durable) {
+  @AutolockWrite
+  public synchronized void setId (I id) {
 
-      if (getId() == null) {
-         if (durable.getId() == null) {
+    this.id = id;
+  }
 
-            return 0;
-         }
-         else {
+  public int compareTo (Durable<I> durable) {
 
-            return -1;
-         }
-      }
-
+    if (getId() == null) {
       if (durable.getId() == null) {
 
-         return 1;
-      }
-
-      return durable.getId().compareTo(getId());
-   }
-
-   @Override
-   @AutolockRead
-   public synchronized int hashCode () {
-
-      if (id == null) {
-
-         return super.hashCode();
-      }
-
-      int h = id.hashCode();
-
-      h ^= (h >>> 20) ^ (h >>> 12);
-
-      return h ^ (h >>> 7) ^ (h >>> 4);
-   }
-
-   @Override
-   public synchronized boolean equals (Object obj) {
-
-      if (obj instanceof Durable) {
-         if ((((Durable)obj).getId() == null) || (id == null)) {
-            return super.equals(obj);
-         }
-         else {
-            return ((Durable)obj).getId().equals(id);
-         }
-      }
-
-      return false;
-   }
-
-   public boolean mirrors (Durable durable) {
-
-      return mirrors(durable, "id");
-   }
-
-   private boolean mirrors (Durable durable, String... exclusions) {
-
-      if (this.getClass().isAssignableFrom(durable.getClass())) {
-
-         boolean excluded;
-
-         try {
-            for (Field field : DurableFields.getFields(this.getClass())) {
-
-               excluded = false;
-
-               if ((exclusions != null) && (exclusions.length > 0)) {
-                  for (String exclusion : exclusions) {
-                     if (exclusion.equals(field.getName())) {
-                        excluded = true;
-                        break;
-                     }
-                  }
-               }
-
-               if (!excluded) {
-
-                  Object myValue = field.get(this);
-                  Object theirValue = field.get(durable);
-
-                  if ((myValue == null)) {
-                     if (theirValue != null) {
-
-                        return false;
-                     }
-                  }
-                  else if (!myValue.equals(theirValue)) {
-
-                     return false;
-                  }
-               }
-            }
-         }
-         catch (IllegalAccessException illegalAccessException) {
-            throw new RuntimeException(illegalAccessException);
-         }
-
-         return true;
-      }
-
-      return false;
-   }
-
-   public String toString () {
-
-      StringBuilder displayBuilder = new StringBuilder();
-
-      if (IN_USE_SET_LOCAL.get().contains(this)) {
-         displayBuilder.append(this.getClass().getSimpleName()).append("[id=").append(id).append(",...]");
+        return 0;
       }
       else {
-         try {
-            IN_USE_SET_LOCAL.get().add(this);
 
-            boolean first = false;
+        return -1;
+      }
+    }
 
-            displayBuilder.append(this.getClass().getSimpleName()).append('[');
+    if (durable.getId() == null) {
 
-            try {
-               for (Field field : DurableFields.getFields(this.getClass())) {
-                  if (first) {
-                     displayBuilder.append(',');
-                  }
+      return 1;
+    }
 
-                  displayBuilder.append(field.getName()).append('=').append(field.get(this));
-                  first = true;
-               }
+    return durable.getId().compareTo(getId());
+  }
+
+  @Override
+  @AutolockRead
+  public synchronized int hashCode () {
+
+    if (id == null) {
+
+      return super.hashCode();
+    }
+
+    int h = id.hashCode();
+
+    h ^= (h >>> 20) ^ (h >>> 12);
+
+    return h ^ (h >>> 7) ^ (h >>> 4);
+  }
+
+  @Override
+  public synchronized boolean equals (Object obj) {
+
+    if (obj instanceof Durable) {
+      if ((((Durable)obj).getId() == null) || (id == null)) {
+        return super.equals(obj);
+      }
+      else {
+        return ((Durable)obj).getId().equals(id);
+      }
+    }
+
+    return false;
+  }
+
+  public boolean mirrors (Durable durable) {
+
+    return mirrors(durable, "id");
+  }
+
+  private boolean mirrors (Durable durable, String... exclusions) {
+
+    if (this.getClass().isAssignableFrom(durable.getClass())) {
+
+      boolean excluded;
+
+      try {
+        for (Field field : DurableFields.getFields(this.getClass())) {
+
+          excluded = false;
+
+          if ((exclusions != null) && (exclusions.length > 0)) {
+            for (String exclusion : exclusions) {
+              if (exclusion.equals(field.getName())) {
+                excluded = true;
+                break;
+              }
             }
-            catch (IllegalAccessException illegalAccessException) {
-               throw new RuntimeException(illegalAccessException);
-            }
+          }
 
-            displayBuilder.append(']');
-         }
-         finally {
-            IN_USE_SET_LOCAL.get().remove(this);
-         }
+          if (!excluded) {
+
+            Object myValue = field.get(this);
+            Object theirValue = field.get(durable);
+
+            if ((myValue == null)) {
+              if (theirValue != null) {
+
+                return false;
+              }
+            }
+            else if (!myValue.equals(theirValue)) {
+
+              return false;
+            }
+          }
+        }
+      }
+      catch (IllegalAccessException illegalAccessException) {
+        throw new RuntimeException(illegalAccessException);
       }
 
-      return displayBuilder.toString();
-   }
+      return true;
+    }
+
+    return false;
+  }
+
+  public String toString () {
+
+    StringBuilder displayBuilder = new StringBuilder();
+
+    if (IN_USE_SET_LOCAL.get().contains(this)) {
+      displayBuilder.append(this.getClass().getSimpleName()).append("[id=").append(id).append(",...]");
+    }
+    else {
+      try {
+        IN_USE_SET_LOCAL.get().add(this);
+
+        boolean first = false;
+
+        displayBuilder.append(this.getClass().getSimpleName()).append('[');
+
+        try {
+          for (Field field : DurableFields.getFields(this.getClass())) {
+            if (first) {
+              displayBuilder.append(',');
+            }
+
+            displayBuilder.append(field.getName()).append('=').append(field.get(this));
+            first = true;
+          }
+        }
+        catch (IllegalAccessException illegalAccessException) {
+          throw new RuntimeException(illegalAccessException);
+        }
+
+        displayBuilder.append(']');
+      }
+      finally {
+        IN_USE_SET_LOCAL.get().remove(this);
+      }
+    }
+
+    return displayBuilder.toString();
+  }
 }
