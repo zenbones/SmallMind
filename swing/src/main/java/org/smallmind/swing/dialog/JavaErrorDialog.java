24c633227dc489b371a64b09a19afc50c1298c89
David Berkman <david.berkman@grenadefish.net>
2011-10-06 11:04:02 -0700

    tiny change

--- swing/src/main/java/org/smallmind/swing/dialog/JavaErrorDialog.java
+++ swing/src/main/java/org/smallmind/swing/dialog/JavaErrorDialog.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -44,6 +44,7 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -51,7 +52,7 @@ import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
 import org.smallmind.nutsnbolts.util.WeakEventListenerList;
 
-public class JavaErrorDialog extends javax.swing.JDialog implements ActionListener, WindowListener {
+public class JavaErrorDialog extends JDialog implements ActionListener, WindowListener {
 
   private static final GridBagLayout GRID_BAG_LAYOUT = new GridBagLayout();
   private static final ImageIcon BUG_ICON = new ImageIcon(ClassLoader.getSystemResource("org/smallmind/swing/dialog/dialog_bug.png"));
