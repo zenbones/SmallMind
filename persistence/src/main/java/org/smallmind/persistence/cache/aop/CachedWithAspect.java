594b132be776368c6d44b5227e10b47ada1056cc
David Berkman <david.berkman@grenadefish.net>
2012-01-18 13:02:57 -0800

    cache invalidation fix

--- persistence/src/main/java/org/smallmind/persistence/cache/aop/CachedWithAspect.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/aop/CachedWithAspect.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -91,7 +91,7 @@ public class CachedWithAspect {
           Iterable<Durable> finderIterable = executeFinder(invalidate.finder(), ormDao, durable);
 
           for (Durable indexingDurable : finderIterable) {
-            vectoredDao.deleteVector(new VectorKey(VectorIndices.getVectorIndexes(invalidate.value(), indexingDurable), invalidate.against().equals(Durable.class) ? durable.getClass() : invalidate.against(), Classifications.get(CachedWith.class, null, invalidate.value())));
+            vectoredDao.deleteVector(new VectorKey(VectorIndices.getVectorIndexes(invalidate.value(), indexingDurable), (invalidate.against() == null) ? durable.getClass() : invalidate.against(), Classifications.get(CachedWith.class, null, invalidate.value())));
           }
         }
       }
@@ -132,7 +132,7 @@ public class CachedWithAspect {
           Iterable<Durable> finderIterable = executeFinder(invalidate.finder(), ormDao, durable);
 
           for (Durable indexingDurable : finderIterable) {
-            vectoredDao.deleteVector(new VectorKey(VectorIndices.getVectorIndexes(invalidate.value(), indexingDurable), invalidate.against().equals(Durable.class) ? durable.getClass() : invalidate.against(), Classifications.get(CachedWith.class, null, invalidate.value())));
+            vectoredDao.deleteVector(new VectorKey(VectorIndices.getVectorIndexes(invalidate.value(), indexingDurable), (invalidate.against() == null) ? durable.getClass() : invalidate.against(), Classifications.get(CachedWith.class, null, invalidate.value())));
           }
         }
       }
