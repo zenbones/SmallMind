36402c679af4e0796e84efb7533df1d567731fb2
David Berkman <david.berkman@grenadefish.net>
2012-01-27 11:38:24 -0800

    threading and concurrency fix

--- quorum/src/main/java/org/smallmind/quorum/pool/connection/DeconstructionQueue.java
+++ quorum/src/main/java/org/smallmind/quorum/pool/connection/DeconstructionQueue.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -41,7 +41,11 @@ public class DeconstructionQueue {
 
   public void startup () {
 
-    new Thread(ignitionWorker = new IgnitionWorker()).start();
+    Thread ignitionThread;
+
+    ignitionThread = new Thread(ignitionWorker = new IgnitionWorker());
+    ignitionThread.setDaemon(true);
+    ignitionThread.start();
   }
 
   public int nextOrdinal () {
@@ -86,7 +90,7 @@ public class DeconstructionQueue {
           IgnitionKey nowKey = new IgnitionKey(Integer.MAX_VALUE, System.currentTimeMillis());
           IgnitionKey fuseKey;
 
-          while (((fuseKey = fuseMap.firstKey()) != null) && fuseKey.compareTo(nowKey) < 0) {
+          while ((!fuseMap.isEmpty()) && ((fuseKey = fuseMap.firstKey()).compareTo(nowKey) < 0)) {
             fuseMap.remove(fuseKey).ignite();
           }
         }
