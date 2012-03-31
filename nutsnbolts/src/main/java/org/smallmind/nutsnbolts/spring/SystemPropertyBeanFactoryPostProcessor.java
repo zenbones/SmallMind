858130a20ef191165e9daa583ad4975ca6f4bff9
David Berkman <david.berkman@grenadefish.net>
2012-03-30 17:35:50 -0700

    correction to capture all settings

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyBeanFactoryPostProcessor.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyBeanFactoryPostProcessor.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -91,10 +91,14 @@ public class SystemPropertyBeanFactoryPostProcessor implements BeanFactoryPostPr
     for (Map.Entry<String, String> propertyEntry : propertyMap.entrySet()) {
       if (override || ((System.getProperty(propertyEntry.getKey()) == null) && (System.getenv(propertyEntry.getKey()) == null))) {
         System.setProperty(propertyEntry.getKey(), propertyEntry.getValue());
-        if ((keyDebugger != null) && keyDebugger.willDebug()) {
-          if (keyDebugger.matches(propertyEntry.getKey())) {
-            debugMap.put(propertyEntry.getKey(), propertyEntry.getValue());
-          }
+      }
+
+      if ((keyDebugger != null) && keyDebugger.willDebug()) {
+        if (keyDebugger.matches(propertyEntry.getKey())) {
+
+          String value;
+
+          debugMap.put(propertyEntry.getKey(), ((value = System.getProperty(propertyEntry.getKey())) != null) ? value : System.getenv(propertyEntry.getKey()));
         }
       }
     }
