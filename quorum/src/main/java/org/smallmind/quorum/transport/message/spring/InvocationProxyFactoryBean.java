691cad579b86daa2e9dafd45253739d2199278fd
David Berkman <david.berkman@grenadefish.net>
2012-04-20 13:28:41 -0700

    spring bean for service proxy injection

---
+++ quorum/src/main/java/org/smallmind/quorum/transport/message/spring/InvocationProxyFactoryBean.java
@@ -0,0 +1,12 @@
+package org.smallmind.quorum.transport.message.spring;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 4/20/12
+ * Time: 1:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvocationProxyFactoryBean {
+
+}
