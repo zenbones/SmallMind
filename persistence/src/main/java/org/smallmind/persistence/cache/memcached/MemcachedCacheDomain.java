ec51a1885e590e0ba8eac2cdeffc70f915553dc9
David Berkman <david.berkman@grenadefish.net>
2012-05-29 17:23:09 -0700

    fix for null time to live override map

--- persistence/src/main/java/org/smallmind/persistence/cache/memcached/MemcachedCacheDomain.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/memcached/MemcachedCacheDomain.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -98,7 +98,7 @@ public class MemcachedCacheDomain<I extends Comparable<I>, D extends Durable<I>>
 
     Integer timeToLiveOverrideSeconds;
 
-    if ((timeToLiveOverrideSeconds = timeTiLiveOverrideMap.get(managedClass)) != null) {
+    if ((timeTiLiveOverrideMap != null) && ((timeToLiveOverrideSeconds = timeTiLiveOverrideMap.get(managedClass)) != null)) {
 
       return timeToLiveOverrideSeconds;
     }
