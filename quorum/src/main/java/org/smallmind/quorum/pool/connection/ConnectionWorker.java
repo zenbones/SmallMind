43dfcc45b0002778c0614b1c204dace98ff9ad0b
David Berkman <david.berkman@grenadefish.net>
2012-06-05 12:35:42 -0700

    minor pooling improvements

--- quorum/src/main/java/org/smallmind/quorum/pool/connection/ConnectionWorker.java
+++ quorum/src/main/java/org/smallmind/quorum/pool/connection/ConnectionWorker.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -55,9 +55,9 @@ public class ConnectionWorker<C> implements Runnable {
     throws Exception {
 
     if (!stateRef.compareAndSet(null, State.ABORTED)) {
-      if (State.TERMINATED.equals(stateRef.get())) {
-        terminationLatch.await();
+      terminationLatch.await();
 
+      if (State.TERMINATED.equals(stateRef.get())) {
         throw exception;
       }
 
@@ -83,7 +83,8 @@ public class ConnectionWorker<C> implements Runnable {
         this.exception = exception;
       }
     }
-
-    terminationLatch.countDown();
+    finally {
+      terminationLatch.countDown();
+    }
   }
 }
