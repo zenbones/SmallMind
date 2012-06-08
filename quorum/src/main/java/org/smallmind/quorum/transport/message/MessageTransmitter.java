95c9c80340c55efe10dcbbf89b5aa189063da155
David Berkman <david.berkman@grenadefish.net>
2012-06-08 15:40:45 -0700

    message transport concurrency optimization

--- quorum/src/main/java/org/smallmind/quorum/transport/message/MessageTransmitter.java
+++ quorum/src/main/java/org/smallmind/quorum/transport/message/MessageTransmitter.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -129,8 +129,14 @@ public class MessageTransmitter {
     try {
 
       AsynchronousTransmissionCallback previousCallback;
+      String correlationId;
 
-      if ((previousCallback = (AsynchronousTransmissionCallback)callbackMap.putIfAbsent(responseMessage.getJMSCorrelationID(), new SynchronousTransmissionCallback(messageStrategy, responseMessage))) != null) {
+      if ((previousCallback = (AsynchronousTransmissionCallback)callbackMap.get(correlationId = responseMessage.getJMSCorrelationID())) == null) {
+        if ((previousCallback = (AsynchronousTransmissionCallback)callbackMap.putIfAbsent(correlationId, new SynchronousTransmissionCallback(messageStrategy, responseMessage))) != null) {
+          previousCallback.setResponseMessage(responseMessage);
+        }
+      }
+      else {
         previousCallback.setResponseMessage(responseMessage);
       }
     }
