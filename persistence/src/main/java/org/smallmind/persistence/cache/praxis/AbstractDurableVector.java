491158a20218c12440b5eed0139a373a57b76aa0
David Berkman <david.berkman@grenadefish.net>
2012-06-12 13:08:29 -0700

    cache operation optimization (do nothing on extrinsic add/remove that does not add or remove anything)

--- persistence/src/main/java/org/smallmind/persistence/cache/praxis/AbstractDurableVector.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/praxis/AbstractDurableVector.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -53,7 +53,9 @@ public abstract class AbstractDurableVector<I extends Serializable & Comparable<
   }
 
   @AutolockWrite
-  public synchronized void add (D durable) {
+  public synchronized boolean add (D durable) {
+
+    boolean changed = false;
 
     if (durable != null) {
 
@@ -75,6 +77,7 @@ public abstract class AbstractDurableVector<I extends Serializable & Comparable<
               inserted = true;
             }
             else {
+              changed = true;
               rosterIter.remove();
             }
 
@@ -89,6 +92,7 @@ public abstract class AbstractDurableVector<I extends Serializable & Comparable<
         }
 
         if (!inserted) {
+          changed = true;
           getRoster().add(index, durable);
         }
       }
@@ -104,24 +108,33 @@ public abstract class AbstractDurableVector<I extends Serializable & Comparable<
         }
 
         if (!matched) {
+          changed = true;
           getRoster().addFirst(durable);
         }
       }
 
       if ((getMaxSize() > 0) && (getRoster().size() > getMaxSize())) {
+        changed = true;
         getRoster().removeLast();
       }
     }
+
+    return changed;
   }
 
   @AutolockWrite
-  public synchronized void remove (D durable) {
+  public synchronized boolean remove (D durable) {
 
+    boolean changed = false;
     boolean removed;
 
     do {
-      removed = getRoster().remove(durable);
+      if (removed = getRoster().remove(durable)) {
+        changed = true;
+      }
     } while (removed);
+
+    return changed;
   }
 
   @AutolockRead
--- persistence/src/main/java/org/smallmind/persistence/cache/praxis/extrinsic/ByKeyExtrinsicCacheDao.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/praxis/extrinsic/ByKeyExtrinsicCacheDao.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with The SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -107,7 +107,9 @@ public class ByKeyExtrinsicCacheDao<I extends Serializable & Comparable<I>, D ex
         }
 
         vectorCopy = (!getVectorCache(vectorKey.getElementClass()).requiresCopyOnDistributedCASOperation()) ? null : (casValue.getValue() == null) ? null : casValue.getValue().copy();
-        casValue.getValue().add(durable);
+        if (!casValue.getValue().add(durable)) {
+          break;
+        }
       } while (!getVectorCache(vectorKey.getElementClass()).putViaCas(vectorKey.getKey(), vectorCopy, casValue.getValue(), casValue.getVersion(), casValue.getValue().getTimeToLiveSeconds()));
     }
   }
@@ -129,7 +131,9 @@ public class ByKeyExtrinsicCacheDao<I extends Serializable & Comparable<I>, D ex
         }
 
         vectorCopy = (!getVectorCache(vectorKey.getElementClass()).requiresCopyOnDistributedCASOperation()) ? null : (casValue.getValue() == null) ? null : casValue.getValue().copy();
-        casValue.getValue().remove(durable);
+        if (!casValue.getValue().remove(durable)) {
+          break;
+        }
       } while (!getVectorCache(vectorKey.getElementClass()).putViaCas(vectorKey.getKey(), vectorCopy, casValue.getValue(), casValue.getVersion(), casValue.getValue().getTimeToLiveSeconds()));
     }
   }
