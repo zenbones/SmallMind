83759559372ccc2a7651d1c6be072cd919de7963
David Berkman <david.berkman@grenadefish.net>
2012-07-16 11:20:21 -0700

    singleton statistics factory

---
+++ persistence/src/main/java/org/smallmind/persistence/statistics/SingletonStatisticsFactory.java
@@ -0,0 +1,12 @@
+package org.smallmind.persistence.statistics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 7/16/12
+ * Time: 11:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SingletonStatisticsFactory {
+
+}
