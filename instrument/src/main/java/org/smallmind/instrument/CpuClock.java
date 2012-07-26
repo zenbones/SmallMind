4aeb3b1d79baa76cd9db5f45945fce1b12fc650d
David Berkman <david.berkman@grenadefish.net>
2012-07-26 15:28:45 -0700

    naming fix (again)

---
+++ instrument/src/main/java/org/smallmind/instrument/CpuClock.java
@@ -0,0 +1,12 @@
+package org.smallmind.instrument;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 7/26/12
+ * Time: 3:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CpuClock {
+
+}
