d6c07b80f8691a3728f515f7617031c2784e5942
David Berkman <david.berkman@grenadefish.net>
2012-09-04 18:48:43 -0700

    naming fubar

---
+++ quorum/src/main/java/org/smallmind/quorum/pool/complex/UnReturnedElementTimeoutDeconstructionFuse.java
@@ -0,0 +1,12 @@
+package org.smallmind.quorum.pool.complex;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 9/4/12
+ * Time: 6:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnReturnedElementTimeoutDeconstructionFuse {
+
+}
---
+++ quorum/src/main/java/org/smallmind/quorum/transport/message/JmsAcknowledgeMode.java
@@ -0,0 +1,12 @@
+package org.smallmind.quorum.transport.message;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 9/4/12
+ * Time: 6:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JmsAcknowledgeMode {
+
+}
