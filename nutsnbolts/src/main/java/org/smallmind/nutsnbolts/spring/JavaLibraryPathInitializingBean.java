df6d5ecb8b4708f01e57f98cec809e3f549069a5
David Berkman <david.berkman@grenadefish.net>
2012-10-18 17:33:26 -0700

    library path setting with spring

---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/JavaLibraryPathInitializingBean.java
@@ -0,0 +1,12 @@
+package org.smallmind.nutsnbolts.spring;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 10/18/12
+ * Time: 5:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaLibraryPathInitializingBean {
+
+}
