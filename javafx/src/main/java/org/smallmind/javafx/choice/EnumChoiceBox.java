d8c07dd17e4ef51d3ffef9dda129aa58201b1836
David Berkman <david.berkman@grenadefish.net>
2012-11-05 10:32:50 -0800

    javafx enum choice box

---
+++ javafx/src/main/java/org/smallmind/javafx/choice/EnumChoiceBox.java
@@ -0,0 +1,12 @@
+package org.smallmind.javafx.choice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 11/5/12
+ * Time: 10:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnumChoiceBox {
+
+}
