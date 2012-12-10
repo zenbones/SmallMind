0e999cb4d0bd3c8dd0ecd5e300a99604e3acd04b
David Berkman <david.berkman@grenadefish.net>
2012-12-10 10:37:43 -0800

    reader input stream

---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/io/ReaderInputStream.java
@@ -0,0 +1,12 @@
+package org.smallmind.nutsnbolts.io;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 12/10/12
+ * Time: 10:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReaderInputStream {
+
+}
