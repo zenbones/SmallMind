3ea409d69905485c0fa1f9d232f4f1fcc4b82de2
David Berkman <david.berkman@grenadefish.net>
2012-12-21 19:05:17 -0800

    websocket client work

---
+++ websocket/src/main/java/org/smallmind/websocket/WebsocketException.java
@@ -0,0 +1,12 @@
+package org.smallmind.websocket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 12/21/12
+ * Time: 12:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebsocketException {
+
+}
---
+++ websocket/src/main/java/org/smallmind/websocket/foo.java
@@ -0,0 +1,12 @@
+package org.smallmind.websocket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 12/21/12
+ * Time: 10:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class foo {
+
+}
