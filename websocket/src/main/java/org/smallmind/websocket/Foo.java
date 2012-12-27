b493c29c4e454a407e4fb0555445aa44670c5882
unknown <David@mustardseed.(none)>
2012-12-27 00:55:49 -0800

    servlet 3.0.1 dependency

--- websocket/src/main/java/org/smallmind/websocket/Foo.java
+++ websocket/src/main/java/org/smallmind/websocket/Foo.java
@@ -26,10 +26,13 @@
  */
 package org.smallmind.websocket;
 
-public class SyntaxException extends WebsocketException {
+import java.net.URI;
 
-  public SyntaxException (String message, Object... args) {
+public class Foo {
 
-    super(message, args);
+  public static void main (String... args)
+    throws Exception {
+
+    new Websocket(URI.create("ws://devg2tc-1.aws.glu.com:8080/game-web-server/websocket"));
   }
 }
