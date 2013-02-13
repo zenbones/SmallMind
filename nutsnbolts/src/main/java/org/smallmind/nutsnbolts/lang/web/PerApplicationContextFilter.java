fb06f87bd055ca05d426c41c7fe0eb7db461d28e
David Berkman <david.berkman@grenadefish.net>
2013-02-12 17:36:45 -0800

    metric context filter

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/lang/web/PerApplicationContextFilter.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/lang/web/PerApplicationContextFilter.java
@@ -24,7 +24,7 @@
  * alone subject to any of the requirements of the GNU Affero GPL
  * version 3.
  */
-package org.smallmind.nutsnbolts.spring.web;
+package org.smallmind.nutsnbolts.lang.web;
 
 import java.io.IOException;
 import javax.servlet.Filter;
