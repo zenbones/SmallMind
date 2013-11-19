a4345e80cfc1acefb58459ccd3353c305519726f
David Berkman <zenbones@grenadefish.net>
2013-11-19 10:25:03 -0800

    moved here from code asylum

---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/security/SHA3.java
@@ -0,0 +1,12 @@
+package org.smallmind.nutsnbolts.security;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 11/19/13
+ * Time: 10:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SHA3 {
+
+}
