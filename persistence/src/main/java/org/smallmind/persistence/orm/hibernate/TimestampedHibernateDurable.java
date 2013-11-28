41e4a60e40651d81062e0c6aaf2575dd59c1c773
David Berkman <zenbones@grenadefish.net>
2013-11-27 20:19:36 -0800

    timestamped hibernate durable

---
+++ persistence/src/main/java/org/smallmind/persistence/orm/hibernate/TimestampedHibernateDurable.java
@@ -0,0 +1,12 @@
+package org.smallmind.persistence.orm.hibernate;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david.berkman
+ * Date: 11/27/13
+ * Time: 8:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TimestampedHibernateDurable {
+
+}
