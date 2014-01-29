5155ed00dd6ca4b7f84fb7a6f8ef7b3e9cf698bc
David Berkman <zenbones@grenadefish.net>
2014-01-29 15:48:44 -0800

    refined for quartz best practice

--- scheduling/quartz/src/main/java/org/smallmind/scheduling/quartz/QuartzProxyJob.java
+++ scheduling/quartz/src/main/java/org/smallmind/scheduling/quartz/QuartzProxyJob.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under the terms of GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the the GNU Affero General Public
  * License, along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -113,12 +113,7 @@ public abstract class QuartzProxyJob implements ProxyJob, Job {
       status = JobStatus.FAILURE;
     }
 
-    if (terminateOnError() && (throwable instanceof Error)) {
-      LoggerManager.getLogger(this.getClass()).fatal(throwable);
-    }
-    else {
-      LoggerManager.getLogger(this.getClass()).error(throwable);
-    }
+    LoggerManager.getLogger(this.getClass()).error(throwable);
   }
 
   @Override
@@ -128,23 +123,22 @@ public abstract class QuartzProxyJob implements ProxyJob, Job {
     startTime = new Date();
     try {
       proceed();
-      shutdown();
     }
     catch (Throwable throwable) {
       setThrowable(throwable);
-
-      if (terminateOnError() && (throwable instanceof Error)) {
-        System.exit(1);
-      }
-      else {
-        throw new JobExecutionException(throwable);
-      }
     }
     finally {
       stopTime = new Date();
 
       if (status.equals(JobStatus.FAILURE) || (count > 0) || logOnZeroCount()) {
         LoggerManager.getLogger(this.getClass()).info("Job(%s) start(%s) stop(%s) count(%d) state(%s)", this.getClass().getSimpleName(), startTime, stopTime, count, status.name());
+      }
+
+      try {
+        cleanup();
+      }
+      catch (Throwable throwable) {
+        LoggerManager.getLogger(this.getClass()).error(throwable);
       }
     }
   }
