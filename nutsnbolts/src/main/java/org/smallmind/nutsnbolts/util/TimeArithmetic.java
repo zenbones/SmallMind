569d5e4e89f67e4dd15c8436d6fe3e84bd791364
David Berkman <david.berkman@grenadefish.net>
2015-02-12 10:06:26 -0800

    file utility

---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/TimeArithmetic.java
@@ -0,0 +1,8 @@
+package org.smallmind.nutsnbolts.util;
+
+/**
+ * Created by david_000 on 2/12/2015.
+ */
+public class TimeArithmetic {
+
+}
---
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/util/TimeOperation.java
@@ -0,0 +1,7 @@
+package org.smallmind.nutsnbolts.util;
+
+/**
+ * Created by david_000 on 2/12/2015.
+ */
+public enum TimeOperation {
+}
