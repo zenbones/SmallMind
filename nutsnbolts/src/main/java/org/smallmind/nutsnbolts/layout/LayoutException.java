66955609270f5aa23b43d833ac36bdca1b968b32
David Berkman <david.berkman@grenadefish.net>
2015-05-04 16:53:37 -0700

    expanded license

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/layout/LayoutException.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/layout/LayoutException.java
@@ -4,18 +4,25 @@
  * This file is part of the SmallMind Code Project.
  * 
  * The SmallMind Code Project is free software, you can redistribute
- * it and/or modify it under the terms of GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ * it and/or modify it under either, at your discretion...
+ * 
+ * 1) The terms of GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * ...or...
+ * 
+ * 2) The terms of the MIT license as published by the Open Source
+ * Initiative
  * 
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * General Public License or MIT License for more details.
  * 
- * You should have received a copy of the the GNU Affero General Public
- * License, along with the SmallMind Code Project. If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * and the MIT License along with the SmallMind Code Project. If not, see
+ * <http://www.gnu.org/licenses/> or <http://opensource.org/licenses/MIT>.
  * 
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
