0859c40d3c37dc9876d3fb854892338066e676f8
David Berkman <david.berkman@grenadefish.net>
2015-06-24 14:25:02 -0700

    snall api change

--- phalanx/src/main/java/org/smallmind/phalanx/wire/WiredService.java
+++ phalanx/src/main/java/org/smallmind/phalanx/wire/WiredService.java
@@ -5,4 +5,7 @@ public interface WiredService {
   public abstract int getVersion ();
 
   public abstract String getServiceName ();
+
+  public abstract void setResponseTransport (ResponseTransport responseTransport)
+    throws Exception;
 }
