3976aaef35d87f528e5bfce5530a18ab32fde9bd
David Berkman <david.berkman@grenadefish.net>
2016-03-15 10:11:00 -0700

    where utility

---
+++ persistence/src/main/java/org/smallmind/persistence/query/WhereUtility.java
@@ -0,0 +1,6 @@
+package org.smallmind.persistence.query;
+
+public class WhereUtility {
+
+  private static void walk (WhereVisitor)
+}
---
+++ persistence/src/main/java/org/smallmind/persistence/query/WhereVisitor.java
@@ -0,0 +1,8 @@
+package org.smallmind.persistence.query;
+
+/**
+ * Created by david on 3/15/2016.
+ */
+public class WhereVisitor {
+
+}
