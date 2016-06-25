413a5ec1f7d91adbf85d63d7580571546a7024d8
David Berkman <david.berkman@grenadefish.net>
2016-06-25 11:49:05 -0700

    cors response filter

--- web/jersey/src/main/java/org/smallmind/web/jersey/cors/CorsResponseFilter.java
+++ web/jersey/src/main/java/org/smallmind/web/jersey/cors/CorsResponseFilter.java
@@ -1,8 +1,23 @@
 package org.smallmind.web.jersey.cors;
 
-/**
- * Created by david on 6/25/2016.
- */
-public class CorsResponseFilter {
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 
+public class CorsResponseFilter implements ContainerResponseFilter {
+
+  @Override
+  public void filter (ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+
+    String accessControlRequestHeaders;
+
+    if (((accessControlRequestHeaders = requestContext.getHeaderString("Access-Control-Request-Headers")) != null) && (!accessControlRequestHeaders.isEmpty())) {
+      responseContext.getHeaders().add("Access-Control-Allow-Headers", accessControlRequestHeaders);
+    }
+
+    responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+    responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, HEAD");
+    responseContext.getHeaders().add("Access-Control-Allow-Credentials", "false");
+  }
 }
+
