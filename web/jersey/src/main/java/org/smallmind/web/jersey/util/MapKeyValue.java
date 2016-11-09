709eb9ed9fcc81480d0208a0e7eb712689da73e4
David Berkman <david.berkman@grenadefish.net>
2016-11-09 08:56:45 -0800

    map xml adapter

---
+++ web/jersey/src/main/java/org/smallmind/web/jersey/util/MapKeyValue.java
@@ -0,0 +1,8 @@
+package org.smallmind.web.jersey.util;
+
+/**
+ * Created by david on 11/9/2016.
+ */
+public class MapKeyValue {
+
+}
--- web/jersey/src/main/java/org/smallmind/web/jersey/util/MapXmlAdapter.java
+++ web/jersey/src/main/java/org/smallmind/web/jersey/util/MapXmlAdapter.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -36,7 +36,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public abstract class SimpleMapXmlAdapter<M extends Map<K, V>, K, V> extends XmlAdapter<LinkedHashMap<?, ?>, M> {
+public abstract class XMapXmlAdapter<M extends Map<K, V>, K, V> extends XmlAdapter<LinkedHashMap<String, Object>, M> {
 
   public abstract M getEmptyMap ();
 
@@ -72,17 +72,19 @@ public abstract class SimpleMapXmlAdapter<M extends Map<K, V>, K, V> extends Xml
   }
 
   @Override
-  public LinkedHashMap<?, ?> marshal (M map) throws Exception {
+  public LinkedHashMap<String, Object> marshal (M map) throws Exception {
 
     if (map != null) {
 
-      LinkedHashMap<K, V> linkedHashMap = new LinkedHashMap<>();
+      LinkedHashMap<String, Object> baseMap = new LinkedHashMap<>();
+      LinkedHashMap<K,V> entryMap = new LinkedHashMap<>();
 
       for (Map.Entry<K, V> entry : map.entrySet()) {
+        baseMap
         linkedHashMap.put(entry.getKey(), entry.getValue());
       }
 
-      return linkedHashMap;
+      return baseMap;
     }
 
     return null;
