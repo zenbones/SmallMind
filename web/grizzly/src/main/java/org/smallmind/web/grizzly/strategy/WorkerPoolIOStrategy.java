1a5059c639b2693640afc576dbb73af9cae45a93
David Berkman <david.berkman@grenadefish.net>
2017-02-23 11:12:59 -0800

    worker pool io strategy fix

--- web/grizzly/src/main/java/org/smallmind/web/grizzly/strategy/WorkerPoolIOStrategy.java
+++ web/grizzly/src/main/java/org/smallmind/web/grizzly/strategy/WorkerPoolIOStrategy.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -34,6 +34,7 @@ package org.smallmind.web.grizzly.strategy;
 
 import java.io.IOException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TransferQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.glassfish.grizzly.Connection;
@@ -45,14 +46,24 @@ import org.glassfish.grizzly.localization.LogMessages;
 import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
 import org.smallmind.instrument.config.MetricConfiguration;
 import org.smallmind.phalanx.worker.WorkManager;
+import org.smallmind.phalanx.worker.WorkerFactory;
 
-public class WorkerPoolIOStrategy extends WorkManager<IOStrategyWorker, IOPackage> implements IOStrategy {
+public class WorkerPoolIOStrategy extends WorkManager<IOStrategyWorker, IOPackage> implements IOStrategy, WorkerFactory<IOStrategyWorker, IOPackage> {
 
   private static final Logger logger = Grizzly.logger(WorkerPoolIOStrategy.class);
 
-  public WorkerPoolIOStrategy (MetricConfiguration metricConfiguration, int concurrencyLimit) {
+  public WorkerPoolIOStrategy (MetricConfiguration metricConfiguration, int concurrencyLimit)
+    throws InterruptedException {
 
     super(metricConfiguration, IOStrategyWorker.class, concurrencyLimit);
+
+    startUp(this);
+  }
+
+  @Override
+  public IOStrategyWorker createWorker (MetricConfiguration metricConfiguration, TransferQueue<IOPackage> transferQueue) {
+
+    return new IOStrategyWorker(metricConfiguration, transferQueue);
   }
 
   @Override
