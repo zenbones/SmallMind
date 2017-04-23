b69cfaa19bf7d4914e98e4c7a463f48902332348
David Berkman <david.berkman@grenadefish.net>
2017-04-23 10:30:56 -0700

    little better code

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyInitializingBean.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/spring/SystemPropertyInitializingBean.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -32,10 +32,8 @@
  */
 package org.smallmind.nutsnbolts.spring;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.SortedMap;
 import java.util.TreeMap;
 import org.smallmind.nutsnbolts.util.AlphaNumericComparator;
 import org.smallmind.nutsnbolts.util.DotNotationException;
@@ -47,21 +45,10 @@ import org.springframework.core.PriorityOrdered;
 public class SystemPropertyInitializingBean implements BeanFactoryPostProcessor, PriorityOrdered {
 
   private final TreeMap<String, String> debugMap = new TreeMap<>(new AlphaNumericComparator<String>());
-  private Map<String, String> propertyMap;
+  private Map<String, String> propertyMap = new HashMap<>();
   private KeyDebugger keyDebugger;
-  private boolean override;
+  private boolean override = false;
   private int order;
-
-  public SystemPropertyInitializingBean () {
-
-    propertyMap = new HashMap<>();
-    override = false;
-  }
-
-  public SortedMap<String, String> getDebugMap () {
-
-    return Collections.unmodifiableSortedMap(debugMap);
-  }
 
   @Override
   public int getOrder () {
