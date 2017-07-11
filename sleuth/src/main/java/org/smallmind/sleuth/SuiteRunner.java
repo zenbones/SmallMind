fd60dc494489ee86dda1fc108d64fa6bd683d048
David Berkman <david.berkman@grenadefish.net>
2017-07-10 20:50:54 -0700

    sleuth 4

--- sleuth/src/main/java/org/smallmind/sleuth/SuiteRunner.java
+++ sleuth/src/main/java/org/smallmind/sleuth/SuiteRunner.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -36,11 +36,13 @@ import java.util.HashSet;
 
 public class SuiteRunner implements Runnable {
 
+  private HashSet<Class<?>> classSet;
   private DependencyQueue<Suite> queue;
   private Dependency<Suite> dependency;
 
   public SuiteRunner (HashSet<Class<?>> classSet, Dependency<Suite> dependency, DependencyQueue<Suite> queue) {
 
+    this.classSet = classSet;
     this.dependency = dependency;
     this.queue = queue;
   }
@@ -48,10 +50,9 @@ public class SuiteRunner implements Runnable {
   @Override
   public void run () {
 
-   try {
-      Thread.sleep(5000);
-    }
-    catch (Exception exception) {
+    try {
+
+    } catch (Exception exception) {
 
     }
     queue.complete(dependency);
