4852290ad0cdc34ce65a731067a105ad3d01e19b
David Berkman <david.berkman@grenadefish.net>
2017-07-14 09:38:01 -0700

    sleuth culprit dependencies
    (cherry picked from commit 22597c50b2f1e17abd25af012fc6837f4262c5d5)

--- sleuth/runner/src/main/java/org/smallmind/sleuth/runner/DependencyQueue.java
+++ sleuth/runner/src/main/java/org/smallmind/sleuth/runner/DependencyQueue.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -33,6 +33,7 @@
 package org.smallmind.sleuth.runner;
 
 import java.lang.annotation.Annotation;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -40,6 +41,7 @@ import java.util.LinkedList;
 public class DependencyQueue<A extends Annotation, T> {
 
   private LinkedList<Dependency<A, T>> dependencyList;
+  private HashMap<String, Dependency<A, T>> dependencyMap;
   private HashSet<String> completedSet = new HashSet<>();
   private int size;
 
@@ -48,6 +50,9 @@ public class DependencyQueue<A extends Annotation, T> {
     this.dependencyList = dependencyList;
 
     size = dependencyList.size();
+    for (Dependency<A, T> dependency : dependencyList) {
+      dependencyMap.put(dependency.getName(), dependency);
+    }
   }
 
   public int size () {
@@ -95,6 +100,9 @@ public class DependencyQueue<A extends Annotation, T> {
         if (!completedSet.contains(requirement)) {
 
           return false;
+        }
+        else if (dependencyMap.get(requirement).getCulprit() != null) {
+          dependency.setCulprit(dependencyMap.get(requirement).getCulprit());
         }
       }
     }
--- sleuth/runner/src/main/java/org/smallmind/sleuth/runner/SuiteRunner.java
+++ sleuth/runner/src/main/java/org/smallmind/sleuth/runner/SuiteRunner.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -78,7 +78,7 @@ public class SuiteRunner implements Runnable {
       AnnotationMethodology<AfterSuite> afterSuiteMethodology;
       AnnotationMethodology<Test> testMethodology;
       CountDownLatch testCompletedLatch;
-      Culprit culprit = null;
+      Culprit culprit = suiteDependency.getCulprit();
       Object instance;
 
       try {
@@ -114,8 +114,10 @@ public class SuiteRunner implements Runnable {
       testCompletedLatch.await();
 
       if ((afterSuiteMethodology = annotationDictionary.getAfterSuiteMethodology()) != null) {
-        afterSuiteMethodology.invoke(sleuthRunner, culprit, suiteDependency.getValue(), instance);
+        culprit = afterSuiteMethodology.invoke(sleuthRunner, culprit, suiteDependency.getValue(), instance);
       }
+
+      suiteDependency.setCulprit(culprit);
     } catch (Exception exception) {
       sleuthRunner.fire(new FatalSleuthEvent(SuiteRunner.class.getName(), "run", System.currentTimeMillis() - startMilliseconds, exception));
     } finally {
