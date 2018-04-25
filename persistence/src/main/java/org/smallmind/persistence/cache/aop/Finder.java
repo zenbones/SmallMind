f3c0f3bc02711af1c255644247442c67c65f6ba9
David Berkman <david.berkman@grenadefish.net>
2018-04-25 10:29:51 -0700

    nada

--- persistence/src/main/java/org/smallmind/persistence/cache/aop/Finder.java
+++ persistence/src/main/java/org/smallmind/persistence/cache/aop/Finder.java
@@ -41,7 +41,7 @@ import org.smallmind.persistence.Durable;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Finder {
 
-  public abstract Class<? extends Durable> with () default Durable.class;
+  Class<? extends Durable> with () default Durable.class;
 
-  public String method () default "";
+  String method () default "";
 }
