46b809aa6e99dcab2bbb23b1d2769f643e64f422
David Berkman <david.berkman@grenadefish.net>
2018-05-24 11:23:56 -0700

    enabled flag for proxy jobs

--- scheduling/quartz/src/main/java/org/smallmind/scheduling/quartz/QuartzProxyJob.java
+++ scheduling/quartz/src/main/java/org/smallmind/scheduling/quartz/QuartzProxyJob.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -36,7 +36,6 @@ import java.util.Date;
 import java.util.LinkedList;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.smallmind.nutsnbolts.util.SuccessOrFailure;
 import org.smallmind.scheduling.base.ProxyJob;
 import org.smallmind.scribe.pen.LoggerManager;
@@ -52,6 +51,12 @@ public abstract class QuartzProxyJob implements ProxyJob, Job {
   public QuartzProxyJob () {
 
     throwableList = new LinkedList<>();
+  }
+
+  @Override
+  public boolean isEnabled () {
+
+    return true;
   }
 
   @Override
@@ -123,25 +128,27 @@ public abstract class QuartzProxyJob implements ProxyJob, Job {
   }
 
   @Override
-  public void execute (JobExecutionContext jobExecutionContext)
-    throws JobExecutionException {
+  public void execute (JobExecutionContext jobExecutionContext) {
 
-    startTime = new Date();
-    try {
-      proceed();
-    } catch (Exception exception) {
-      setThrowable(exception);
-    } finally {
-      stopTime = new Date();
-
-      if (status.equals(SuccessOrFailure.FAILURE) || (count > 0) || logOnZeroCount()) {
-        LoggerManager.getLogger(this.getClass()).info("Job(%s) start(%s) stop(%s) count(%d) state(%s)", this.getClass().getSimpleName(), startTime, stopTime, count, status.name());
-      }
+    if (isEnabled()) {
+      startTime = new Date();
 
       try {
-        cleanup();
+        proceed();
       } catch (Exception exception) {
-        LoggerManager.getLogger(this.getClass()).error(exception);
+        setThrowable(exception);
+      } finally {
+        stopTime = new Date();
+
+        if (status.equals(SuccessOrFailure.FAILURE) || (count > 0) || logOnZeroCount()) {
+          LoggerManager.getLogger(this.getClass()).info("Job(%s) start(%s) stop(%s) count(%d) state(%s)", this.getClass().getSimpleName(), startTime, stopTime, count, status.name());
+        }
+
+        try {
+          cleanup();
+        } catch (Exception exception) {
+          LoggerManager.getLogger(this.getClass()).error(exception);
+        }
       }
     }
   }
