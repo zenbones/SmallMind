7efa86275cc09c907cdddb0900aa89b59f351d2c
David Berkman <david.berkman@grenadefish.net>
2018-06-09 18:29:07 -0700

    ssl jersey test init

---
+++ web/jersey/src/main/java/org/smallmind/web/jersey/aop/SSLJerseyTest.java
@@ -0,0 +1,14 @@
+package org.smallmind.web.jersey.aop;
+
+import org.glassfish.jersey.test.JerseyTest;
+
+public class SSLJerseyTest extends JerseyTest {
+
+  @Override
+  public void setUp () throws Exception {
+
+    super.setUp();
+
+    
+  }
+}
