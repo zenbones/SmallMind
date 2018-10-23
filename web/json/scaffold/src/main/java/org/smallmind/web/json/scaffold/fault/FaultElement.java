5023ab3f08e9c566688ca9fed43eb9f4d0e9ee34
David Berkman <david.berkman@grenadefish.net>
2018-10-22 19:02:51 -0700

    hash and equals fix

--- web/json/scaffold/src/main/java/org/smallmind/web/json/scaffold/fault/FaultElement.java
+++ web/json/scaffold/src/main/java/org/smallmind/web/json/scaffold/fault/FaultElement.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -138,16 +138,11 @@ public class FaultElement implements Serializable {
 
   public int hashCode () {
 
-    int result = 31 * declaringType.hashCode() + functionName.hashCode();
-
-    result = 31 * result + Objects.hashCode(fileName);
-    result = 31 * result + lineNumber;
-
-    return result;
+    return Objects.hash(declaringType, functionName, fileName, lineNumber);
   }
 
   public boolean equals (Object obj) {
 
-    return (obj == this) || ((obj instanceof FaultElement) && ((FaultElement)obj).getDeclaringType().equals(declaringType) && ((FaultElement)obj).getFunctionName().equals(functionName) && ((FaultElement)obj).getFileName().equals(fileName) && (((FaultElement)obj).getLineNumber() == lineNumber));
+    return (obj == this) || ((obj instanceof FaultElement) && Objects.equals(((FaultElement)obj).getDeclaringType(), declaringType) && Objects.equals(((FaultElement)obj).getFunctionName(), functionName) && Objects.equals(((FaultElement)obj).getFileName(), fileName) && (((FaultElement)obj).getLineNumber() == lineNumber));
   }
 }
