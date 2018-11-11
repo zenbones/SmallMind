b823beec34994e12b90e0919c28a611350dae730
David Berkman <david.berkman@grenadefish.net>
2018-11-10 16:46:42 -0800

    allow transcoder

--- memcached/src/main/java/org/smallmind/memcached/spring/XMemcachedMemcachedClientFactoryBean.java
+++ memcached/src/main/java/org/smallmind/memcached/spring/XMemcachedMemcachedClientFactoryBean.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -40,6 +40,7 @@ import net.rubyeye.xmemcached.MemcachedClientBuilder;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.command.BinaryCommandFactory;
 import net.rubyeye.xmemcached.impl.KetamaMemcachedSessionLocator;
+import net.rubyeye.xmemcached.transcoders.Transcoder;
 import org.smallmind.memcached.MemcachedServer;
 import org.smallmind.memcached.XMemcachedMemcachedClient;
 import org.smallmind.scribe.pen.LoggerManager;
@@ -50,6 +51,7 @@ import org.springframework.beans.factory.InitializingBean;
 public class XMemcachedMemcachedClientFactoryBean implements FactoryBean<XMemcachedMemcachedClient>, InitializingBean {
 
   private XMemcachedMemcachedClient memcachedClient;
+  private Transcoder<?> transcoder;
   private MemcachedServer[] servers;
   private MemcachedServer[] backups;
   private boolean enabled = true;
@@ -58,6 +60,11 @@ public class XMemcachedMemcachedClientFactoryBean implements FactoryBean<XMemcac
   public void setEnabled (boolean enabled) {
 
     this.enabled = enabled;
+  }
+
+  public void setTranscoder (Transcoder<?> transcoder) {
+
+    this.transcoder = transcoder;
   }
 
   public void setServers (MemcachedServer[] servers) {
@@ -103,10 +110,16 @@ public class XMemcachedMemcachedClientFactoryBean implements FactoryBean<XMemcac
         }
 
         builder = new XMemcachedClientBuilder(addressMap);
+
+        if (transcoder != null) {
+          builder.setTranscoder(transcoder);
+        }
+
         builder.setFailureMode(true);
         builder.setConnectionPoolSize(poolSize);
         builder.setCommandFactory(new BinaryCommandFactory());
         builder.setSessionLocator(new KetamaMemcachedSessionLocator());
+
         memcachedClient = new XMemcachedMemcachedClient(builder.build());
       }
     }
