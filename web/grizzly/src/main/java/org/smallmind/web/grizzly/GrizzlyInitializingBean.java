201b87e7be45817823ee98c129f833e9be9fbbc0
David Berkman <david.berkman@grenadefish.net>
2019-01-04 22:51:34 -0800

    allow max http header size

--- web/grizzly/src/main/java/org/smallmind/web/grizzly/GrizzlyInitializingBean.java
+++ web/grizzly/src/main/java/org/smallmind/web/grizzly/GrizzlyInitializingBean.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -88,6 +88,7 @@ public class GrizzlyInitializingBean implements DisposableBean, ApplicationConte
   private String restPath = "/rest";
   private String soapPath = "/soap";
   private String webSocketPath = "/websocket";
+  private Integer maxHttpHeaderSize;
   private Integer initialWorkerPoolSize;
   private Integer maximumWorkerPoolSize;
   private int port = 80;
@@ -107,6 +108,11 @@ public class GrizzlyInitializingBean implements DisposableBean, ApplicationConte
   public void setPort (int port) {
 
     this.port = port;
+  }
+
+  public void setMaxHttpHeaderSize (Integer maxHttpHeaderSize) {
+
+    this.maxHttpHeaderSize = maxHttpHeaderSize;
   }
 
   public void setInitialWorkerPoolSize (Integer initialWorkerPoolSize) {
@@ -350,6 +356,9 @@ public class GrizzlyInitializingBean implements DisposableBean, ApplicationConte
 
   private NetworkListener configureNetworkListener (NetworkListener networkListener) {
 
+    if (maxHttpHeaderSize != null) {
+      networkListener.setMaxHttpHeaderSize(maxHttpHeaderSize);
+    }
     if (ioStrategy != null) {
       networkListener.getTransport().setIOStrategy(ioStrategy);
     }
