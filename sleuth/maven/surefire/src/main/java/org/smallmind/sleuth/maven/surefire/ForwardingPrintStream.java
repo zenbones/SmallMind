1728c74ad4e414a7be0444c673125705f4c99348
David Berkman <david.berkman@grenadefish.net>
2019-07-18 10:22:39 -0700

    whatever

--- sleuth/maven/surefire/src/main/java/org/smallmind/sleuth/maven/surefire/ForwardingPrintStream.java
+++ sleuth/maven/surefire/src/main/java/org/smallmind/sleuth/maven/surefire/ForwardingPrintStream.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -33,13 +33,12 @@
 package org.smallmind.sleuth.maven.surefire;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
 import org.apache.maven.surefire.report.ConsoleOutputReceiver;
 
-import static org.apache.maven.surefire.util.internal.StringUtils.NL;
-
 public class ForwardingPrintStream extends PrintStream {
+
+  private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
   private final ConsoleOutputReceiver consoleOutputReceiver;
   private final boolean isStdOut;
@@ -59,8 +58,7 @@ public class ForwardingPrintStream extends PrintStream {
   }
 
   @Override
-  public void write (byte[] b)
-    throws IOException {
+  public void write (byte[] b) {
 
     consoleOutputReceiver.writeTestOutput(b, 0, b.length, isStdOut);
   }
@@ -68,17 +66,13 @@ public class ForwardingPrintStream extends PrintStream {
   @Override
   public void write (int b) {
 
-    try {
-      write(new byte[] {(byte)b});
-    } catch (IOException ioException) {
-      setError();
-    }
+    write(new byte[] {(byte)b});
   }
 
   @Override
   public void println (String s) {
 
-    byte[] bytes = (((s == null) ? "null" : s) + NL).getBytes();
+    byte[] bytes = (((s == null) ? "null" : s) + LINE_SEPARATOR).getBytes();
 
     consoleOutputReceiver.writeTestOutput(bytes, 0, bytes.length, isStdOut);
   }
