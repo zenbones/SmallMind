8dea3f141dc9f26e3f87124a3d91af727d60eadc
David Berkman <david.berkman@grenadefish.net>
2020-01-07 15:34:25 -0800

    wildcard fix

--- web/json/query/src/main/java/org/smallmind/web/json/query/WhereOperator.java
+++ web/json/query/src/main/java/org/smallmind/web/json/query/WhereOperator.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -166,19 +166,19 @@ public enum WhereOperator {
               case 0:
                 return op2.get().equals("");
               case 1:
-                return op1.get().equals("%") || op2.get().equals(op1.get());
+                return op1.get().equals(SINGLE_WILDCARD) || op2.get().equals(op1.get());
               case 2:
-                return op1.get().equals("%%") || (((String)op1.get()).charAt(0) == '%') ? ((String)op2.get()).endsWith(((String)op1.get()).substring(1)) : (((String)op1.get()).charAt(1) == '%') ? ((String)op2.get()).startsWith(((String)op1.get()).substring(0, 1)) : op2.get().equals(op1.get());
+                return op1.get().equals(DOUBLE_WILDCARD) || (((String)op1.get()).charAt(0) == WILDCARD_CHAR) ? ((String)op2.get()).endsWith(((String)op1.get()).substring(1)) : (((String)op1.get()).charAt(1) == WILDCARD_CHAR) ? ((String)op2.get()).startsWith(((String)op1.get()).substring(0, 1)) : op2.get().equals(op1.get());
               default:
-                if (((String)op1.get()).substring(1, ((String)op1.get()).length() - 1).indexOf('%') >= 0) {
-                  throw new QueryProcessingException("The operation(%s) allows wildcards('%') only at the  start or end of the operand", name());
-                } else if (((String)op1.get()).startsWith("%") && ((String)op1.get()).endsWith("%")) {
+                if (((String)op1.get()).substring(1, ((String)op1.get()).length() - 1).indexOf(WILDCARD_CHAR) >= 0) {
+                  throw new QueryProcessingException("The operation(%s) allows wildcards(%s) only at the  start or end of the operand", name(), SINGLE_WILDCARD);
+                } else if (((String)op1.get()).startsWith(SINGLE_WILDCARD) && ((String)op1.get()).endsWith(SINGLE_WILDCARD)) {
 
                   return ((String)op2.get()).contains(((String)op1.get()).substring(1, ((String)op1.get()).length() - 1));
-                } else if (((String)op1.get()).startsWith("%")) {
+                } else if (((String)op1.get()).startsWith(SINGLE_WILDCARD)) {
 
                   return ((String)op2.get()).endsWith(((String)op1.get()).substring(1));
-                } else if (((String)op1.get()).endsWith("%")) {
+                } else if (((String)op1.get()).endsWith(SINGLE_WILDCARD)) {
 
                   return ((String)op2.get()).startsWith(((String)op1.get()).substring(0, ((String)op1.get()).length() - 1));
                 } else {
@@ -246,6 +246,9 @@ public enum WhereOperator {
   };
 
   private static final NumberComparator NUMBER_COMPARATOR = new NumberComparator();
+  private static final String SINGLE_WILDCARD = "*";
+  private static final String DOUBLE_WILDCARD = SINGLE_WILDCARD + SINGLE_WILDCARD;
+  private static final char WILDCARD_CHAR = '*';
 
   public abstract boolean isTrue (WhereOperand<?> op1, WhereOperand<?> op2);
 }
