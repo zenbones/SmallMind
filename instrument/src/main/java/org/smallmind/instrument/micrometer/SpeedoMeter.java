602fbfe777523264b30f3394e388d5968f842b1b
David Berkman <david.berkman@grenadefish.net>
2020-03-25 15:06:10 -0700

    instrument

---
+++ instrument/src/main/java/org/smallmind/instrument/micrometer/SpeedoMeter.java
@@ -0,0 +1,19 @@
+package org.smallmind.instrument.micrometer;
+
+import io.micrometer.core.instrument.Measurement;
+import io.micrometer.core.instrument.Meter;
+
+public class SpeedoMeter implements Meter {
+
+  @Override
+  public Id getId () {
+
+    return null;
+  }
+
+  @Override
+  public Iterable<Measurement> measure () {
+
+    return null;
+  }
+}
