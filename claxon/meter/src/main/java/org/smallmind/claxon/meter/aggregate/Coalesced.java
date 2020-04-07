8312d213fdbc5207ef3ab999f7ca324dac194373
David Berkman <david.berkman@grenadefish.net>
2020-04-06 19:02:01 -0700

    claxon

--- claxon/meter/src/main/java/org/smallmind/claxon/meter/aggregate/Coalesced.java
+++ claxon/meter/src/main/java/org/smallmind/claxon/meter/aggregate/Coalesced.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -44,16 +44,16 @@ public class Coalesced extends AbstractAggregate {
   private final ReentrantLock lock = new ReentrantLock();
   private final ConcurrentLinkedQueue<Long> valueQueue = new ConcurrentLinkedQueue<>();
   private final AtomicInteger size = new AtomicInteger();
-  private final double nanosecondsInVelocity;
+  private final double nanosecondsInWindow;
   private final double nanosecondsInPulse;
   private double average = 0;
   private long markTime;
 
-  public Coalesced (String name, TimeUnit velocityTimeUnit, Stint pulseStint) {
+  public Coalesced (String name, TimeUnit windowTimeUnit, Stint pulseStint) {
 
     super(name);
 
-    nanosecondsInVelocity = StintUtility.convertToDouble(1, velocityTimeUnit, TimeUnit.NANOSECONDS);
+    nanosecondsInWindow = StintUtility.convertToDouble(1, windowTimeUnit, TimeUnit.NANOSECONDS);
     nanosecondsInPulse = pulseStint.getTimeUnit().toNanos(pulseStint.getTime());
     markTime = System.nanoTime();
   }
@@ -79,19 +79,19 @@ public class Coalesced extends AbstractAggregate {
           Long unprocessed;
           long accumulated = value;
           int cap = size.get();
-          int index = 0;
+          int n = 0;
 
           if (cap > 0) {
             while ((unprocessed = valueQueue.poll()) != null) {
               size.decrementAndGet();
               accumulated += unprocessed;
-              if (++index >= cap) {
+              if (++n >= cap) {
                 break;
               }
             }
           }
 
-          average = ((double)accumulated) / (transpired / nanosecondsInVelocity);
+          average = ((double)accumulated) / (transpired / nanosecondsInWindow);
 
           markTime = now;
         }
@@ -104,6 +104,10 @@ public class Coalesced extends AbstractAggregate {
   }
 
   public double getAverage () {
+
+    if (size.get() > 0) {
+      process(0);
+    }
 
     return average;
   }
--- claxon/meter/src/main/java/org/smallmind/claxon/meter/aggregate/ExponentiallyWeightedMovingAverage.java
+++ claxon/meter/src/main/java/org/smallmind/claxon/meter/aggregate/ExponentiallyWeightedMovingAverage.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -55,13 +55,13 @@ public class ExponentiallyWeightedMovingAverage {
 
   public void update (long value) {
 
-    if (!process(value)) {
+    if (!process(value, true)) {
       size.incrementAndGet();
       valueQueue.add(value);
     }
   }
 
-  private boolean process (long value) {
+  private boolean process (long value, boolean required) {
 
     if (lock.tryLock()) {
       try {
@@ -83,12 +83,15 @@ public class ExponentiallyWeightedMovingAverage {
           }
         }
 
-        nPlusOne = n + 1;
+        if (required | (n > 0)) {
 
-        if (markTime == 0) {
-          average = ((double)accumulated) / nPlusOne;
-        } else {
-          average += (1 - Math.exp(-((now - markTime) / nanosecondsInWindow))) * ((((double)accumulated) / nPlusOne) - average);
+          nPlusOne = (required) ? n + 1 : n;
+
+          if (markTime == 0) {
+            average = ((double)accumulated) / nPlusOne;
+          } else {
+            average += (1 - Math.exp(-((now - markTime) / nanosecondsInWindow))) * ((((double)accumulated) / nPlusOne) - average);
+          }
         }
 
         markTime = now;
@@ -104,6 +107,8 @@ public class ExponentiallyWeightedMovingAverage {
   }
 
   public double getMovingAverage () {
+
+    process(0, false);
 
     return average;
   }
