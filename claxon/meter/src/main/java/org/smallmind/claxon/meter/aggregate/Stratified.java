2e3fa19b6075609b4e8b3f827353786a06eef040
David Berkman <david.berkman@grenadefish.net>
2020-04-08 15:38:03 -0700

    claxon

--- claxon/meter/src/main/java/org/smallmind/claxon/meter/aggregate/Stratified.java
+++ claxon/meter/src/main/java/org/smallmind/claxon/meter/aggregate/Stratified.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -41,12 +41,9 @@ import org.smallmind.nutsnbolts.time.StintUtility;
 public class Stratified extends AbstractAggregate {
 
   private final ReentrantLock updateLock = new ReentrantLock();
-  private final ReentrantLock flipLock = new ReentrantLock(true);
   private final double nanosecondsInVelocity;
   private Recorder writeRecorder;
   private Recorder readRecorder;
-  private Histogram readHistogram;
-  private Histogram writeHistogram;
   private long markTime;
 
   public Stratified () {
@@ -105,19 +102,10 @@ public class Stratified extends AbstractAggregate {
         if ((now - markTime) > nanosecondsInVelocity) {
 
           Recorder recorder = readRecorder;
-          Histogram histogram = readHistogram;
 
-          flipLock.lock();
-          try {
-            readRecorder = writeRecorder;
-            readHistogram = writeHistogram;
-          } finally {
-            flipLock.unlock();
-          }
-
+          readRecorder = writeRecorder;
           recorder.reset();
           writeRecorder = recorder;
-          writeHistogram = histogram;
 
           markTime = now;
         }
@@ -133,12 +121,6 @@ public class Stratified extends AbstractAggregate {
       checkForReset();
     }
 
-    flipLock.lock();
-    try {
-
-      return readHistogram = readRecorder.getIntervalHistogram(readHistogram);
-    } finally {
-      flipLock.unlock();
-    }
+    return readRecorder.getIntervalHistogram();
   }
 }
