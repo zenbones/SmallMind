89e053e1d99b266739d4bd31b4cfe9418edcbe14
David Berkman <david.berkman@grenadefish.net>
2020-04-13 16:43:05 -0700

    meters first

--- claxon/registry/src/main/java/org/smallmind/claxon/registry/ClaxonRegistry.java
+++ claxon/registry/src/main/java/org/smallmind/claxon/registry/ClaxonRegistry.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -165,31 +165,31 @@ public class ClaxonRegistry {
     measurableTrcker.sweepAndUpdate();
     observableTracker.sweep();
 
-    for (Collector collector : collectorMap.values()) {
-      for (Map.Entry<RegistryKey, Meter> meterEntry : meterMap.entrySet()) {
-        try {
+    for (Map.Entry<RegistryKey, Meter> meterEntry : meterMap.entrySet()) {
 
-          Quantity[] quantities = meterEntry.getValue().record();
+      Quantity[] quantities = meterEntry.getValue().record();
 
-          if ((quantities != null) && (quantities.length > 0)) {
+      if ((quantities != null) && (quantities.length > 0)) {
 
-            Tag[] mergedTags;
-            Tag[] meterTags = meterEntry.getKey().getTags();
+        Tag[] mergedTags;
+        Tag[] meterTags = meterEntry.getKey().getTags();
 
-            if ((registryTags == null) || (registryTags.length == 0)) {
-              mergedTags = meterTags;
-            } else if ((meterTags == null) || (meterTags.length == 0)) {
-              mergedTags = registryTags;
-            } else {
-              mergedTags = new Tag[registryTags.length + meterTags.length];
-              System.arraycopy(registryTags, 0, mergedTags, 0, registryTags.length);
-              System.arraycopy(meterTags, 0, mergedTags, registryTags.length, meterTags.length);
-            }
+        if ((registryTags == null) || (registryTags.length == 0)) {
+          mergedTags = meterTags;
+        } else if ((meterTags == null) || (meterTags.length == 0)) {
+          mergedTags = registryTags;
+        } else {
+          mergedTags = new Tag[registryTags.length + meterTags.length];
+          System.arraycopy(registryTags, 0, mergedTags, 0, registryTags.length);
+          System.arraycopy(meterTags, 0, mergedTags, registryTags.length, meterTags.length);
+        }
 
+        for (Collector collector : collectorMap.values()) {
+          try {
             collector.record(meterEntry.getKey().getIdentifier(), mergedTags, quantities);
+          } catch (Exception exception) {
+            LoggerManager.getLogger(ClaxonRegistry.class).error(exception);
           }
-        } catch (Exception exception) {
-          LoggerManager.getLogger(ClaxonRegistry.class).error(exception);
         }
       }
     }
