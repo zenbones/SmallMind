6b07e5af370b76285d17a708badfc52352c5bcb1
David Berkman <david.berkman@grenadefish.net>
2020-05-23 05:02:17 -0700

    prometheus fix

--- claxon/collector/prometheus/src/main/java/org/smallmind/claxon/collector/prometheus/PrometheusCollector.java
+++ claxon/collector/prometheus/src/main/java/org/smallmind/claxon/collector/prometheus/PrometheusCollector.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -70,9 +70,9 @@ public class PrometheusCollector extends PullCollector<String> {
   @Override
   public void record (String meterName, Tag[] tags, Quantity[] quantities) {
 
-    StringBuilder identityBuilder = mangle(meterName, new StringBuilder());
+    StringBuilder identityBuilder = mangle(meterName, new StringBuilder(), false);
     StringBuilder labelBuilder = null;
-    long nowInSeconds = System.currentTimeMillis() / 1000;
+    long nowInMilliseconds = System.currentTimeMillis();
 
     if ((tags != null) && (tags.length > 0)) {
 
@@ -84,7 +84,7 @@ public class PrometheusCollector extends PullCollector<String> {
         if (!first) {
           labelBuilder.append(',');
         }
-        mangle(tag.getKey(), labelBuilder).append("=\"").append(tag.getValue()).append('"');
+        mangle(tag.getKey(), labelBuilder, true).append("=\"").append(tag.getValue()).append('"');
         first = false;
       }
       labelBuilder.append("}");
@@ -94,11 +94,11 @@ public class PrometheusCollector extends PullCollector<String> {
 
       StringBuilder rowBuilder = new StringBuilder(identityBuilder);
 
-      mangle(quantity.getName(), rowBuilder.append('_'));
+      mangle(quantity.getName(), rowBuilder.append('_'), false);
       if (labelBuilder != null) {
         rowBuilder.append(labelBuilder);
       }
-      rowBuilder.append(' ').append(quantity.getValue()).append(' ').append(nowInSeconds);
+      rowBuilder.append(' ').append(quantity.getValue()).append(' ').append(nowInMilliseconds);
 
       if (rowCount.incrementAndGet() > maxRows) {
         if (rowQueue.poll() != null) {
@@ -130,7 +130,7 @@ public class PrometheusCollector extends PullCollector<String> {
   }
 
   // Being Golang, Prometheus can't handle unicode strings like most frameworks, but only a very simple set of characters.
-  private StringBuilder mangle (String original, StringBuilder mangledBuilder) {
+  private StringBuilder mangle (String original, StringBuilder mangledBuilder, boolean label) {
 
     Letter state = Letter.UNKNOWN;
 
@@ -150,7 +150,7 @@ public class PrometheusCollector extends PullCollector<String> {
         state = Letter.DIGIT;
       } else {
         if (singleChar == '.') {
-          singleChar = '-';
+          singleChar = label ? '_' : '-';
         } else if (singleChar != '-') {
           singleChar = '_';
         }
