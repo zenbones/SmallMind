a984f0d436014e7541be1de9d64de22b792da795
David Berkman <david.berkman@grenadefish.net>
2020-07-29 11:25:50 -0700

    binary data

--- nutsnbolts/src/main/java/org/smallmind/nutsnbolts/json/BinaryData.java
+++ nutsnbolts/src/main/java/org/smallmind/nutsnbolts/json/BinaryData.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -46,6 +46,7 @@ public class BinaryData implements Serializable {
 
   private Encoding encoding;
   private Encryption encryption;
+  private String name;
   private String contentType;
   private String data;
 
@@ -76,14 +77,6 @@ public class BinaryData implements Serializable {
     this.encryption = encryption;
 
     data = encoding.encode(encryption.encrypt(key, original.getBytes()));
-  }
-
-  public BinaryData (String contentType, Encoding encoding, Encryption encryption, Key key, String original)
-    throws Exception {
-
-    this(encoding, encryption, key, original);
-
-    this.contentType = contentType;
   }
 
   public byte[] decode ()
@@ -130,15 +123,30 @@ public class BinaryData implements Serializable {
     this.encryption = encryption;
   }
 
+  @XmlElement(name = "name")
+  public String getName () {
+
+    return name;
+  }
+
+  public BinaryData setName (String name) {
+
+    this.name = name;
+
+    return this;
+  }
+
   @XmlElement(name = "contentType")
   public String getContentType () {
 
     return contentType;
   }
 
-  public void setContentType (String contentType) {
+  public BinaryData setContentType (String contentType) {
 
     this.contentType = contentType;
+
+    return this;
   }
 
   @XmlElement(name = "data", required = true)
