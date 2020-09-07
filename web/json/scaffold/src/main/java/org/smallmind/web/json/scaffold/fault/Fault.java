31cd48c43ae2b0adda04bde88aa4a7fd97edc352
David Berkman <david.berkman@grenadefish.net>
2020-09-07 09:06:53 -0700

    better

--- web/json/scaffold/src/main/java/org/smallmind/web/json/scaffold/fault/Fault.java
+++ web/json/scaffold/src/main/java/org/smallmind/web/json/scaffold/fault/Fault.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -184,7 +184,7 @@ public class Fault implements Serializable {
 
   public <T> T readInformation (Class<T> clazz) {
 
-    return (information == null) ? null : JsonCodec.convert(information, clazz);
+    return (information == null) ? null : JsonNode.class.equals(clazz) ? clazz.cast(information) : JsonCodec.convert(information, clazz);
   }
 
   public Fault writeInformation (Object obj) {
