2a92c81f7bd70ed2a1382531b779acd29570ab1c
David Berkman <david.berkman@grenadefish.net>
2020-09-08 15:05:57 -0700

    this is handled already

--- persistence/src/main/java/org/smallmind/persistence/orm/hibernate/MySQL5InnoDBDialect.java
+++ persistence/src/main/java/org/smallmind/persistence/orm/hibernate/MySQL5InnoDBDialect.java
@@ -1,28 +1,28 @@
 /*
  * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
- * 
+ *
  * This file is part of the SmallMind Code Project.
- * 
+ *
  * The SmallMind Code Project is free software, you can redistribute
  * it and/or modify it under either, at your discretion...
- * 
+ *
  * 1) The terms of GNU Affero General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * ...or...
- * 
+ *
  * 2) The terms of the Apache License, Version 2.0.
- * 
+ *
  * The SmallMind Code Project is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License or Apache License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * and the Apache License along with the SmallMind Code Project. If not, see
  * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
- * 
+ *
  * Additional permission under the GNU Affero GPL version 3 section 7
  * ------------------------------------------------------------------
  * If you modify this Program, or any covered work, by linking or
@@ -34,7 +34,6 @@ package org.smallmind.persistence.orm.hibernate;
 
 import java.sql.Types;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
-import org.hibernate.type.IntegerType;
 import org.hibernate.type.StandardBasicTypes;
 import org.hibernate.type.TimestampType;
 
@@ -45,7 +44,6 @@ public class MySQL5InnoDBDialect extends org.hibernate.dialect.MySQL5InnoDBDiale
     super();
 
     registerColumnType(Types.BOOLEAN, "BIT(1)");
-    registerFunction("org_smallmind_day_diff", new SQLFunctionTemplate(IntegerType.INSTANCE, "datediff(?2, ?1)"));
     registerFunction("org_smallmind_date_sub_minutes", new SQLFunctionTemplate(TimestampType.INSTANCE, "date_sub(?1, INTERVAL ?2 MINUTE)"));
     this.registerFunction("org_smallmind_group_concat", new SQLFunctionTemplate(StandardBasicTypes.STRING, "group_concat(?1)"));
   }
