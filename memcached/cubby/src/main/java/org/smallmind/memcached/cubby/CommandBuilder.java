4c79b1b264f4b8ef45b55e5d5740ca7cae9c9917
David Berkman <david.berkman@grenadefish.net>
2022-01-10 13:42:12 -0800

    cubby

---
+++ memcached/cubby/src/main/java/org/smallmind/memcached/cubby/CommandBuilder.java
@@ -0,0 +1,2 @@
+package org.smallmind.memcached.cubby;public class CommandBuilder {
+}
--- memcached/cubby/src/main/java/org/smallmind/memcached/cubby/CubbyMemcachedClient.java
+++ memcached/cubby/src/main/java/org/smallmind/memcached/cubby/CubbyMemcachedClient.java
@@ -1,5 +1,24 @@
 package org.smallmind.memcached.cubby;
 
+import java.util.concurrent.TimeUnit;
+import org.smallmind.nutsnbolts.time.Stint;
+
 public class CubbyMemcachedClient {
 
+  private final Stint timeoutStint;
+
+  public CubbyMemcachedClient () {
+
+    this(new Stint(3, TimeUnit.SECONDS));
+  }
+
+  public CubbyMemcachedClient (Stint timeoutStint) {
+
+    this.timeoutStint = timeoutStint;
+  }
+
+  public Stint getTimeoutStint () {
+
+    return timeoutStint;
+  }
 }
