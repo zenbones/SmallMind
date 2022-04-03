735f4619f4dac05fbd7e7a0abaccf99a6854f766
David Berkman <david.berkman@grenadefish.net>
2022-04-03 15:42:33 -0700

    ephemeral path

--- file/ephemeral/src/main/java/org/smallmind/file/ephemeral/EphemeralPath.java
+++ file/ephemeral/src/main/java/org/smallmind/file/ephemeral/EphemeralPath.java
@@ -41,13 +41,15 @@ import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class EphemeralPath implements Path {
 
+  private static final String[] NO_NAMES = new String[0];
   private static final String SEPARATOR = "/";
   private final EphemeralFileSystem fileSystem;
-  private final LinkedList<String> nameList = new LinkedList<>();
+  private final String[] names;
   private final boolean absolute;
 
   protected EphemeralPath (EphemeralFileSystem fileSystem) {
@@ -55,6 +57,7 @@ public class EphemeralPath implements Path {
     this.fileSystem = fileSystem;
 
     absolute = true;
+    names = NO_NAMES;
   }
 
   public EphemeralPath (EphemeralFileSystem fileSystem, String first, String... more) {
@@ -64,26 +67,40 @@ public class EphemeralPath implements Path {
     if (first == null) {
       throw new NullPointerException();
     } else {
+
+      LinkedList<String> nameList = new LinkedList<>();
+
       absolute = first.startsWith(SEPARATOR);
 
-      append(first, absolute);
+      split(nameList, first, absolute);
 
       if ((more != null) && (more.length > 0)) {
         for (String another : more) {
-          append(another, false);
+          split(nameList, another, false);
         }
       }
+
+      names = nameList.toArray(new String[0]);
     }
   }
 
   private EphemeralPath (EphemeralPath path, int begin, int end) {
 
-    fileSystem = path.fileSystem;
-    absolute = path.absolute;
+    fileSystem = (EphemeralFileSystem)path.getFileSystem();
+    absolute = path.isAbsolute();
+    names = new String[end - begin];
+    int to = 0;
 
-    for (int index = begin; index < end; index++) {
-      nameList.add(path.nameList.get(index));
+    for (int from = begin; from < end; from++) {
+      names[to++] = path.getNames()[from];
     }
+  }
+
+  private EphemeralPath (EphemeralFileSystem fileSystem, String[] names, boolean absolute) {
+
+    this.fileSystem = fileSystem;
+    this.names = names;
+    this.absolute = absolute;
   }
 
   public static char getSeparatorChar () {
@@ -96,7 +113,7 @@ public class EphemeralPath implements Path {
     return SEPARATOR;
   }
 
-  private void append (String text, boolean absolute) {
+  private void split (LinkedList<String> nameList, String text, boolean absolute) {
 
     if (text.length() == 0) {
       throw new InvalidPathException(text, "Empty path component");
@@ -106,14 +123,20 @@ public class EphemeralPath implements Path {
 
       for (String segment : text.split(SEPARATOR, -1)) {
         if (segment.length() == 0) {
-          if (absolute && (index > 0))
+          if ((!absolute) || (index > 0)) {
             throw new InvalidPathException(text, "Empty path component");
+          }
         } else {
           nameList.add(segment);
           index++;
         }
       }
     }
+  }
+
+  public String[] getNames () {
+
+    return names;
   }
 
   @Override
@@ -137,35 +160,35 @@ public class EphemeralPath implements Path {
   @Override
   public Path getFileName () {
 
-    return nameList.isEmpty() ? null : new EphemeralPath(fileSystem, nameList.getLast());
+    return (names.length == 0) ? null : new EphemeralPath(fileSystem, names[names.length - 1]);
   }
 
   @Override
   public Path getParent () {
 
-    return nameList.isEmpty() ? null : (nameList.size() > 1) ? new EphemeralPath(this, 0, nameList.size() - 1) : absolute ? new EphemeralPath(fileSystem) : null;
+    return (names.length == 0) ? null : (names.length > 1) ? new EphemeralPath(this, 0, names.length - 1) : absolute ? new EphemeralPath(fileSystem) : null;
   }
 
   @Override
   public int getNameCount () {
 
-    return nameList.size();
+    return names.length;
   }
 
   @Override
   public Path getName (int index) {
 
-    if ((index < 0) || (index >= nameList.size())) {
+    if ((index < 0) || (index >= names.length)) {
       throw new IllegalArgumentException("Illegal index value");
     }
 
-    return new EphemeralPath(fileSystem, nameList.get(index));
+    return new EphemeralPath(fileSystem, names[index]);
   }
 
   @Override
   public Path subpath (int beginIndex, int endIndex) {
 
-    if ((beginIndex < 0) || (beginIndex >= nameList.size()) || (endIndex <= beginIndex) || (endIndex > nameList.size())) {
+    if ((beginIndex < 0) || (beginIndex >= names.length) || (endIndex <= beginIndex) || (endIndex > names.length)) {
       throw new IllegalArgumentException("Illegal index value");
     } else {
 
@@ -176,11 +199,40 @@ public class EphemeralPath implements Path {
   @Override
   public boolean startsWith (Path other) {
 
+    if ((other.getFileSystem() instanceof EphemeralFileSystem) && (other.getNameCount() <= names.length)) {
+      for (int index = 0; index < other.getNameCount(); index++) {
+        if (!((EphemeralPath)other).getNames()[index].equals(names[index])) {
+
+          return false;
+        }
+      }
+
+      return true;
+    }
+
     return false;
   }
 
   @Override
   public boolean endsWith (Path other) {
+
+    if (other.getFileSystem() instanceof EphemeralFileSystem) {
+      if (absolute || (!other.isAbsolute())) {
+        if ((other.isAbsolute() && (other.getNameCount() == names.length)) || ((!other.isAbsolute()) && (other.getNameCount() <= names.length))) {
+
+          int offset = names.length - other.getNameCount();
+
+          for (int index = 0; index < other.getNameCount(); index++) {
+            if (!((EphemeralPath)other).getNames()[index].equals(names[offset + index])) {
+
+              return false;
+            }
+          }
+
+          return true;
+        }
+      }
+    }
 
     return false;
   }
@@ -188,7 +240,33 @@ public class EphemeralPath implements Path {
   @Override
   public Path normalize () {
 
-    return null;
+    LinkedList<String> namesList = null;
+    int position = 0;
+
+    for (String name : names) {
+      switch (name) {
+        case ".":
+          if (namesList == null) {
+            namesList = new LinkedList<>(Arrays.asList(names).subList(0, position));
+          }
+          break;
+        case "..":
+          if (namesList == null) {
+            namesList = (position > 1) ? new LinkedList<>(Arrays.asList(names).subList(0, position - 1)) : new LinkedList<>();
+          } else if (!namesList.isEmpty()) {
+            namesList.removeLast();
+          }
+          break;
+        default:
+          if (namesList == null) {
+            position++;
+          } else {
+            namesList.add(name);
+          }
+      }
+    }
+
+    return (namesList == null) ? this : new EphemeralPath(fileSystem, namesList.toArray(new String[0]), absolute);
   }
 
   @Override
