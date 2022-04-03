f034f1ae14db7eebf72f547a6543a03f862ca52f
David Berkman <david.berkman@grenadefish.net>
2022-04-03 11:47:30 -0700

    ephemeralfs

--- file/ephemeral/src/main/java/org/smallmind/file/ephemeral/EphemeralFileSystem.java
+++ file/ephemeral/src/main/java/org/smallmind/file/ephemeral/EphemeralFileSystem.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 David Berkman
+ *
+ * This file is part of the SmallMind Code Project.
+ *
+ * The SmallMind Code Project is free software, you can redistribute
+ * it and/or modify it under either, at your discretion...
+ *
+ * 1) The terms of GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * ...or...
+ *
+ * 2) The terms of the Apache License, Version 2.0.
+ *
+ * The SmallMind Code Project is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License or Apache License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and the Apache License along with the SmallMind Code Project. If not, see
+ * <http://www.gnu.org/licenses/> or <http://www.apache.org/licenses/LICENSE-2.0>.
+ *
+ * Additional permission under the GNU Affero GPL version 3 section 7
+ * ------------------------------------------------------------------
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with other code, such other code is not for that reason
+ * alone subject to any of the requirements of the GNU Affero GPL
+ * version 3.
+ */
 package org.smallmind.file.ephemeral;
 
 import java.nio.file.FileStore;
@@ -14,15 +46,18 @@ import org.smallmind.nutsnbolts.util.SingleItemIterable;
 public class EphemeralFileSystem extends FileSystem {
 
   private static final EphemeralUserPrincipalLookupService USER_PRINCIPAL_LOOKUP_SERVICE = new EphemeralUserPrincipalLookupService();
-  private static final EphemeralPath ROOT_PATH = new EphemeralPath("/");
   private static final String SEPARATOR = String.valueOf(EphemeralPath.getSeparator());
-  private final EphemeralFileStore fileStore = new EphemeralFileStore();
+  private final EphemeralPath rootPath;
+  private final EphemeralFileStore fileStore;
   private final EphemeralFileSystemProvider provider;
   private volatile boolean closed;
 
   public EphemeralFileSystem (EphemeralFileSystemProvider provider) {
 
     this.provider = provider;
+
+    fileStore = new EphemeralFileStore(0, 0);
+    rootPath = new EphemeralPath(this, SEPARATOR);
   }
 
   @Override
@@ -58,7 +93,7 @@ public class EphemeralFileSystem extends FileSystem {
   @Override
   public Iterable<Path> getRootDirectories () {
 
-    return ROOT_PATH;
+    return rootPath;
   }
 
   public EphemeralFileStore getFileStore () {
@@ -81,7 +116,7 @@ public class EphemeralFileSystem extends FileSystem {
   @Override
   public Path getPath (String first, String... more) {
 
-    return new EphemeralPath(first, more);
+    return new EphemeralPath(this, first, more);
   }
 
   @Override
