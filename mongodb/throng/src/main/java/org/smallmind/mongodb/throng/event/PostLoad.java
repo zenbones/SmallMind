822418bcbe6c8f44da484638379362ed4987652d
David Berkman <david.berkman@grenadefish.net>
2023-04-05 21:18:06 -0700

    throng

---
+++ mongodb/throng/src/main/java/org/smallmind/mongodb/throng/event/PostLoad.java
@@ -0,0 +1,13 @@
+package org.smallmind.mongodb.throng.event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+// public method () {}
+public @interface PostLoad {
+
+}
