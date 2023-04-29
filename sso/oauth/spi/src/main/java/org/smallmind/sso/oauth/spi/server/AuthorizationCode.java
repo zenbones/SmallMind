e18d98adee7096536beee1ac34df58c5f318f2fa
David Berkman <david.berkman@grenadefish.net>
2023-04-28 17:56:02 -0700

    oauth

--- sso/oauth/spi/src/main/java/org/smallmind/sso/oauth/spi/server/AuthorizationCode.java
+++ sso/oauth/spi/src/main/java/org/smallmind/sso/oauth/spi/server/AuthorizationCode.java
@@ -6,13 +6,15 @@ public class AuthorizationCode {
 
   private final String clientId;
   private final String requestUri;
+  private final String scope;
   private final byte[] nonce;
   private final long timestamp;
 
-  public AuthorizationCode (String clientId, String requestUri) {
+  public AuthorizationCode (String clientId, String requestUri, String scope) {
 
     this.clientId = clientId;
     this.requestUri = requestUri;
+    this.scope = scope;
 
     nonce = new byte[4];
     ThreadLocalRandom.current().nextBytes(nonce);
