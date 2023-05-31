068913a8279cc94214b8f0abdce443939c9a298f
David Berkman <david.berkman@grenadefish.net>
2023-05-30 21:05:02 -0700

    oauth

---
+++ sso/oauth/jersey/src/test/java/org/smallmind/sso/oauth/v2dot0/OAuthResourceTest.java
@@ -0,0 +1,13 @@
+package org.smallmind.sso.oauth.v2dot0;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.testng.annotations.Test;
+
+@Test
+public class OAuthResourceTest extends JerseyTest {
+
+  @Test
+  public void test () {
+
+  }
+}
