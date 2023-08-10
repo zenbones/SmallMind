bb34fe35d241a8b5f686d883bc0d6d2fd56f5e4a
David Berkman <david.berkman@grenadefish.net>
2023-08-10 11:23:30 -0700

    oumaumua

---
+++ bayeux/oumuamua/src/main/java/org/smallmind/bayeux/oumuamua/spi/backbone/kafka/KafkaBackbone.java
@@ -0,0 +1,12 @@
+package org.smallmind.bayeux.oumuamua.spi.backbone.kafka;
+
+import org.smallmind.bayeux.oumuamua.api.Message;
+import org.smallmind.bayeux.oumuamua.api.backbone.Backbone;
+
+public class KafkaBackbone implements Backbone {
+
+  @Override
+  public void publish (Message message) {
+
+  }
+}
