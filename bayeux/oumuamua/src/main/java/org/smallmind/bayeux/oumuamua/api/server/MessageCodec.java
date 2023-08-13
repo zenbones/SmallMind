2aed8d4664b586528bd11702285c5622d0b302b3
David Berkman <david.berkman@grenadefish.net>
2023-08-13 11:29:02 -0700

    oumuamua

--- bayeux/oumuamua/src/main/java/org/smallmind/bayeux/oumuamua/api/server/MessageCodec.java
+++ bayeux/oumuamua/src/main/java/org/smallmind/bayeux/oumuamua/api/server/MessageCodec.java
@@ -1,21 +1,17 @@
 package org.smallmind.bayeux.oumuamua.api.server;
 
-import java.nio.charset.Charset;
 import org.smallmind.bayeux.oumuamua.api.Body;
 import org.smallmind.bayeux.oumuamua.api.Message;
 import org.smallmind.bayeux.oumuamua.api.MessageType;
 
+// Valid json encodings are all versions of UTF, but this specification uses Java standard UTF-8
 public interface MessageCodec {
 
   byte[] encode (Body<?> body);
 
-  byte[] encode (Body<?> body, Charset charset);
-
   String toJson (Body<?> body);
 
   Message decode (MessageType messageType, byte[] buffer);
-
-  Message decode (MessageType messageType, byte[] buffer, Charset charset);
 
   Message createMessage (MessageType messageType);
 
