018f22ad704049bd1444b3d06f66ccabaa482485
David Berkman <david.berkman@grenadefish.net>
2024-09-25 13:35:33 -0700

    note

--- bayeux/oumuamua/server/impl/src/main/java/org/smallmind/bayeux/oumuamua/server/impl/OumuamuaSession.java
+++ bayeux/oumuamua/server/impl/src/main/java/org/smallmind/bayeux/oumuamua/server/impl/OumuamuaSession.java
@@ -253,6 +253,7 @@ public class OumuamuaSession<V extends Value<V>> extends AbstractAttributed impl
   public void deliver (Channel<V> fromChannel, Session<V> sender, Packet<V> packet) {
 
     if (SessionState.CONNECTED.equals(stateRef.get())) {
+      // steaming ignores the ack extension, *if* the protocol does not require long polling
       if (fromChannel.isStreaming() && (!connectionRef.get().getTransport().getProtocol().isLongPolling())) {
 
         Packet<V> processedPacket;
